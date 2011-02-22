5a29d341613e471a97b56fa5189545beb9ea843b
Olivier Grisel <ogrisel@apache.org>
2011-02-22 14:39:47 +0000

    STANBOL-91: more work on the new defaultdata package to distributed the required OpenNLP models
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1073345 13f79535-47bb-0310-9956-ffa450edef68

--- defaultdata/download_models.sh
+++ defaultdata/download_models.sh
@@ -1,12 +1,12 @@
 #/bin/sh
 
-OPENNLP_DATA=src/main/resources/opennlp
+OPENNLP_DATA=src/main/resources/org/apache/stanbol/defaultdata/opennlp
 MODELS_URL="http://opennlp.sourceforge.net/models-1.5"
 
 
 rm -rf $OPENNLP_DATA/*.bin
 
-(cd $OPENNLP_DATA && wget $MODELS_URL/en-token.bin)
+(cd $OPENNLP_DATA && wget $MODELS_URL/en-sent.bin)
 (cd $OPENNLP_DATA && wget $MODELS_URL/en-ner-person.bin)
 (cd $OPENNLP_DATA && wget $MODELS_URL/en-ner-location.bin)
 (cd $OPENNLP_DATA && wget $MODELS_URL/en-ner-organization.bin)
