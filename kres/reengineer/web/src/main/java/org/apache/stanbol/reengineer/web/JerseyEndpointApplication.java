d7c9c3dad715443f9e04d2f038f521437d56e11d
Bonafede Concetto Elvio <concelvio@apache.org>
2011-03-20 16:18:44 +0000

    STANBOL-39: 1) Created the module rules/web and adding required pom.xml and src directory 2) Moved class SemionReengineerREsource.java from eu.iksproject.kres.jersey to org.apache.stanbol.reengieneer.web (module reengineer/web) 3) Added JerseyEndpoint.java, JerseyEndpointApplication.java and JettyServer.java
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1083485 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kres/reengineer/web/src/main/java/org/apache/stanbol/reengineer/web/JerseyEndpointApplication.java
@@ -0,0 +1,49 @@
+package org.apache.stanbol.reengineer.web;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.apache.stanbol.kres.jersey.processors.KReSViewProcessor;
+import org.apache.stanbol.kres.jersey.writers.GraphWriter;
+import org.apache.stanbol.kres.jersey.writers.OWLOntologyWriter;
+import org.apache.stanbol.kres.jersey.writers.ResultSetWriter;
+
+/**
+ * Statically define the list of available resources and providers to be used by the KReS JAX-RS Endpoint.
+ * 
+ * The jersey auto-scan mechanism does not seem to work when deployed through OSGi's HttpService
+ * initialization.
+ * 
+ * In the future this class might get refactored as an OSGi service to allow for dynamic configuration and
+ * deployment of additional JAX-RS resources and providers.
+ * 
+ * @author andrea.nuzzolese
+ */
+
+public class JerseyEndpointApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+
+        classes.add(SemionReengineerResource.class);
+        
+        // message body writers
+        classes.add(GraphWriter.class);
+        classes.add(ResultSetWriter.class);
+        // classes.add(OwlModelWriter.class);
+        classes.add(OWLOntologyWriter.class);
+        return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> singletons = new HashSet<Object>();
+        // view processors
+        singletons.add(new KReSViewProcessor());
+        return singletons;
+    }
+
+}
