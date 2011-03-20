260088d9411950bea22fbc60ea7b0ecfb452f77e
Bonafede Concetto Elvio <concelvio@apache.org>
2011-03-20 16:25:45 +0000

    STANBOL-39: 1) Created the module rules/web and adding required pom.xml and src directory 2) Moved classes ONMOntResource.java, ONRootResource.java, ONMScopeOntologyResource.java, ONMScopeResource.java from eu.iksproject.kres.jersey to org.apache.stanbol.ontologymanager.web (module ontologymanager/web) 3) Added JerseyEndpoint.java, JerseyEndpointApplication.java and JettyServer.java
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1083487 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kres/ontologymanager/web/src/main/java/org/apache/stanbol/ontologymanager/web/ONMOntResource.java
@@ -0,0 +1,123 @@
+package org.apache.stanbol.ontologymanager.web;
+
+import java.util.Hashtable;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import org.apache.clerezza.rdf.core.access.TcManager;
+import org.apache.clerezza.rdf.core.serializedform.Serializer;
+import org.apache.stanbol.kres.jersey.resource.NavigationMixin;
+import org.apache.stanbol.ontologymanager.ontonet.api.KReSONManager;
+import org.apache.stanbol.ontologymanager.ontonet.api.ontology.OntologyIndex;
+import org.apache.stanbol.ontologymanager.ontonet.impl.ONManager;
+import org.apache.stanbol.ontologymanager.ontonet.impl.ontology.OntologyStorage;
+import org.coode.owlapi.turtle.TurtleOntologyFormat;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.RDFXMLOntologyFormat;
+import org.semanticweb.owlapi.io.StringDocumentTarget;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/ontology/get")
+public class ONMOntResource extends NavigationMixin {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /*
+     * Placeholder for the KReSONManager to be fetched from the servlet context.
+     */
+    protected KReSONManager onm;
+    protected OntologyStorage storage;
+
+    protected ServletContext servletContext;
+
+    protected Serializer serializer;
+
+    public ONMOntResource(@Context ServletContext servletContext) {
+        this.servletContext = servletContext;
+        this.onm = (KReSONManager) servletContext.getAttribute(KReSONManager.class.getName());
+//      this.storage = (OntologyStorage) servletContext
+//      .getAttribute(OntologyStorage.class.getName());
+// Contingency code for missing components follows.
+/*
+ * FIXME! The following code is required only for the tests. This should
+ * be removed and the test should work without this code.
+ */
+if (onm == null) {
+    log
+            .warn("No KReSONManager in servlet context. Instantiating manually...");
+    onm = new ONManager(new TcManager(), null,
+            new Hashtable<String, Object>());
+}
+this.storage = onm.getOntologyStore();
+if (storage == null) {
+    log.warn("No OntologyStorage in servlet context. Instantiating manually...");
+    storage = new OntologyStorage(new TcManager(),null);
+}
+        serializer = (Serializer) this.servletContext.getAttribute(Serializer.class.getName());
+    }
+
+    @GET
+    @Produces("application/rdf+xml")
+    public Response getOntology(@QueryParam("iri") String ontologyIri) {
+
+        IRI iri = null;
+        try {
+            iri = IRI.create(ontologyIri);
+        } catch (Exception ex) {
+            throw new WebApplicationException(404);
+        }
+        OntologyIndex index = onm.getOntologyIndex();
+        if (!index.isOntologyLoaded(iri))
+        // No such ontology registered, so return 404.
+        return Response.status(404).build();
+
+        OWLOntology ont = index.getOntology(iri);
+        OWLOntologyManager tmpmgr = OWLManager.createOWLOntologyManager();
+        StringDocumentTarget tgt = new StringDocumentTarget();
+        try {
+            tmpmgr.saveOntology(ont, new RDFXMLOntologyFormat(), tgt);
+        } catch (OWLOntologyStorageException e) {
+            throw new WebApplicationException(500);
+        }
+        return Response.ok(tgt.toString()).build();
+    }
+
+    @GET
+    @Produces("text/turtle")
+    public Response getOntologyT(@QueryParam("iri") String ontologyIri) {
+
+        IRI iri = null;
+        try {
+            iri = IRI.create(ontologyIri);
+        } catch (Exception ex) {
+            throw new WebApplicationException(404);
+        }
+        OntologyIndex index = onm.getOntologyIndex();
+        if (!index.isOntologyLoaded(iri))
+        // No such ontology registered, so return 404.
+        return Response.status(404).build();
+
+        OWLOntology ont = index.getOntology(iri);
+        OWLOntologyManager tmpmgr = OWLManager.createOWLOntologyManager();
+        StringDocumentTarget tgt = new StringDocumentTarget();
+        try {
+            tmpmgr.saveOntology(ont, new TurtleOntologyFormat(), tgt);
+        } catch (OWLOntologyStorageException e) {
+            throw new WebApplicationException(500);
+        }
+        return Response.ok(tgt.toString()).build();
+    }
+
+}
