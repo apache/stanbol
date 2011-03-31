02689fc905ebe1a1c0fb1082dc48d2c5baa49f9e
Bertrand Delacretaz <bdelacretaz@apache.org>
2011-03-31 13:04:17 +0000

    Add missing Apache License
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/enhancer@1087287 13f79535-47bb-0310-9956-ffa450edef68

--- jersey/src/main/java/org/apache/stanbol/enhancer/jersey/JerseyEndpointApplication.java
+++ jersey/src/main/java/org/apache/stanbol/enhancer/jersey/JerseyEndpointApplication.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.stanbol.enhancer.jersey;
 
 import java.util.HashSet;
@@ -6,6 +22,7 @@ import java.util.Set;
 import javax.ws.rs.core.Application;
 
 import org.apache.stanbol.enhancer.jersey.processors.FreemarkerViewProcessor;
+import org.apache.stanbol.enhancer.jersey.resource.BenchmarkRootResource;
 import org.apache.stanbol.enhancer.jersey.resource.EnginesRootResource;
 import org.apache.stanbol.enhancer.jersey.resource.EnhancerRootResource;
 import org.apache.stanbol.enhancer.jersey.resource.SparqlQueryResource;
@@ -35,6 +52,7 @@ public class JerseyEndpointApplication extends Application {
         classes.add(EnginesRootResource.class);
         classes.add(StoreRootResource.class);
         classes.add(SparqlQueryResource.class);
+        classes.add(BenchmarkRootResource.class);
 
         // message body writers
         classes.add(GraphWriter.class);
