e59e6f1c4b54149f56f2cbd1be19a4f0d6823ea4
Olivier Grisel <ogrisel@apache.org>
2011-04-04 13:02:49 +0000

    STANBOL-120: manual merge of the http-endpoint-refactoring branch as of r1088457 (I could not get the svn merge work as expected because of the many package renamings)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/commons/web/base@1088603 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/stanbol/commons/web/base/BundleHttpContext.java
@@ -0,0 +1,43 @@
+package org.apache.stanbol.commons.web.base;
+
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.service.http.HttpContext;
+
+/**
+ * Custom HTTP Context to lookup the resources from the classloader of the OSGi bundle.
+ */
+public class BundleHttpContext implements HttpContext {
+
+    private Bundle bundle;
+
+    public BundleHttpContext(WebFragment fragment) {
+        this.bundle = fragment.getBundleContext().getBundle();
+    }
+
+    public BundleHttpContext(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    public String getMimeType(String name) {
+        // someone in the chain seems to already be doing the Mime type mapping
+        return null;
+    }
+
+    public URL getResource(String name) {
+        if (name.startsWith("/")) {
+            name = name.substring(1);
+        }
+
+        return this.bundle.getResource(name);
+    }
+
+    public boolean handleSecurity(HttpServletRequest req, HttpServletResponse res) {
+        return true;
+    }
+
+}
