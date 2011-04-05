0d9e24f0c41f6c109691b11c04e7ece1f28c1f2f
Stefane Fermigier <sfermigier@apache.org>
2011-04-05 14:51:54 +0000

    Remove unneeded keywords.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/commons/web/base@1089070 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/stanbol/commons/web/base/WebFragment.java
+++ src/main/java/org/apache/stanbol/commons/web/base/WebFragment.java
@@ -19,7 +19,7 @@ public interface WebFragment {
      * Name of the fragment. Should be a lowercase short name without any kind of special character, so as to
      * be used as a path component in the URL of the static resources.
      */
-    public String getName();
+    String getName();
 
     /**
      * Java package name that is the classloading root of the static resources of the fragment to be published
@@ -27,7 +27,7 @@ public interface WebFragment {
      * 
      * Note: this package should be exported by the bundle.
      */
-    public String getStaticResourceClassPath();
+    String getStaticResourceClassPath();
 
     /**
      * Set of JAX-RS resources provided as classes.
@@ -35,7 +35,7 @@ public interface WebFragment {
      * Note: those classes should be visible: use the Export-Package bundle declaration to export their
      * packages.
      */
-    public Set<Class<?>> getJaxrsResourceClasses();
+    Set<Class<?>> getJaxrsResourceClasses();
 
     /**
      * Set of JAX-RS resources provided as singleton instances.
@@ -43,14 +43,14 @@ public interface WebFragment {
      * Note: those objects should be visible: use the Export-Package bundle declaration to export their
      * packages.
      */
-    public Set<Object> getJaxrsResourceSingletons();
+    Set<Object> getJaxrsResourceSingletons();
 
     /**
      * @return a template load instance that can be used by the FreemarkerViewProcessor for building the HTML
      *         UI incrementally. If this is an instance of ClassTemplateLoader, the class path visibility
      *         should be exported using the Export-Package bundle declaration.
      */
-    public TemplateLoader getTemplateLoader();
+    TemplateLoader getTemplateLoader();
 
     /**
      * CSS and favicon resources to be linked in the head of all HTML pages controlled by the NavigationMixin
@@ -58,7 +58,7 @@ public interface WebFragment {
      * 
      * ${it.staticRootUrl}/${link.fragmentName}/${link.relativePath}
      */
-    public List<LinkResource> getLinkResources();
+    List<LinkResource> getLinkResources();
 
     /**
      * Javascript resources to be linked in the head of all HTML pages controlled by the NavigationMixin
@@ -66,17 +66,17 @@ public interface WebFragment {
      * 
      * ${it.staticRootUrl}/${script.fragmentName}/${script.relativePath}
      */
-    public List<ScriptResource> getScriptResources();
+    List<ScriptResource> getScriptResources();
 
     /**
      * List of link descriptions to contribute to the main navigation menu.
      */
-    public List<NavigationLink> getNavigationLinks();
+    List<NavigationLink> getNavigationLinks();
 
     /**
      * @return the bundle context who contributed this fragment (useful for loading the resources from the
      *         right classloading context)
      */
-    public BundleContext getBundleContext();
+    BundleContext getBundleContext();
 
 }
