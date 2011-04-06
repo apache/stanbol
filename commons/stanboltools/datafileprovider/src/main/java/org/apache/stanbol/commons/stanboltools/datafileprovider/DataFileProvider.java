9352ae5db2f82c3559a4ed2cca7bedd872ee5cb8
Bertrand Delacretaz <bdelacretaz@apache.org>
2011-04-06 13:25:46 +0000

    STANBOL-146 - DataFileProvider service (WIP, no OSGi console plugin yet)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1089445 13f79535-47bb-0310-9956-ffa450edef68

---
+++ commons/stanboltools/datafileprovider/src/main/java/org/apache/stanbol/commons/stanboltools/datafileprovider/DataFileProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.stanbol.commons.stanboltools.datafileprovider;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Used to provide read-only data files (indexes, models etc.) from various
+ * locations (bundle resources, filesystem folders etc.) allowing users to
+ * overrides default data files with their own.
+ * 
+ * See STANBOL-146 for requirements.
+ */
+public interface DataFileProvider {
+    /** Get the InputStream of the specified
+     *  data file, according to this provider's
+     *  priority rules.
+     *  
+     *  @param bundleSymbolicName can be used to differentiate 
+     *      between files which have the same name
+     *  @param filename name of the file to open
+     *  @param downloadExplanation explains how to get the "full"
+     *      version of the data file, in case we don't find it or in
+     *      case we supply a default small variant.
+     * @return InputStream to read the file, must be closed by 
+     *      caller when done
+     * @throws IOException problem finding or reading the file
+     */
+    InputStream getInputStream(
+            String bundleSymbolicName, 
+            String filename,
+            String downloadExplanation) throws IOException;
+}
