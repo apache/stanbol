76cd35bd6d3e742cddf3b65a359e473fc20c2903
Rupert Westenthaler <rwesten@apache.org>
2011-04-06 13:14:26 +0000

    This revision adds support to install ReferencedSites for the Entityhub by adding a Bundle to the OSGI Web Console.
    STANBOL-141: Adds Support for installing SolrIndexes provided as archives with the name <indexName>.solrindex.[<compressionFormat>] where
      - indexname is the name of the core added to the EmbeddedSolrServer managed by the SolrYard
      - compressionFormat is the format used for the archive. ZIP is the default.
    Open:
     - support for uninstalling Solr Indexes
     
     The implementation is part of the SolrYard Bundle.
     
     
     STANBOL-140: Adds Support for installing resources contained in Bundles that define the "Install-Path" header in there manifest.
     Open:
      - remove configuration if a Bundle is uninstalled
      - update the configuration if an bundle is updated
      - no support for Jars.
      
     The implementation can be found in"/commons/installer/bundleprovider" 
      
      
    other Changes
    
    SolrYard:
     - added the functionality to add SolrIndexes to the SolrDirectoryManager
     - moved the ConfigUtils to the impl package because it is no longer intended to be used from outside of the SolrYard bundle
     - Text Queries for multiple words now create "Word1 AND Word2 AND Wordn" type SolrQueries. Previously "Word1+Word2+Wordn" was used. This had not worked with the the Tokenizers used for  such fields. Queries in STR field (that do not use Tokenizers) still use the old pattern.
     - adapted unit test
     
     Stanboltools/Offline
      - the pom.xml now uses the parent
      
     Enhancer/Full launcher
      - added the BundleInstaller: Now it is possible to install ReferencedSites for the Entityhub by adding a Bundle to the OSGI Web Console
     
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/commons@1089443 13f79535-47bb-0310-9956-ffa450edef68

---
+++ installer/bundleprovider/src/main/java/org/apache/stanbol/commons/installer/provider/bundle/impl/BundleInstaller.java
@@ -0,0 +1,238 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.stanbol.commons.installer.provider.bundle.impl;
+
+import static org.apache.stanbol.commons.installer.provider.bundle.BundleInstallerConstants.BUNDLE_INSTALLER_HEADER;
+import static org.apache.stanbol.commons.installer.provider.bundle.BundleInstallerConstants.PROVIDER_SCHEME;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.sling.installer.api.InstallableResource;
+import org.apache.sling.installer.api.OsgiInstaller;
+import org.apache.sling.installer.api.tasks.ResourceTransformer;
+import org.apache.stanbol.commons.installer.provider.bundle.BundleInstallerConstants;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Installs resources within bundles by using the Apache Sling Installer
+ * framework.<p>
+ * NOTE that currently installed resources are not removed if the bundle is
+ * deactivated because it is not clear if this is a good thing to do. maybe one
+ * should use {@link Bundle#UNINSTALLED} for that. However this needs some
+ * additional testing.
+ * <p>
+ * The OSGi extender pattern (as described at [1]) is used. The value of the 
+ * {@link BundleInstallerConstants#BUNDLE_INSTALLER_HEADER} 
+ * ({@value BundleInstallerConstants#BUNDLE_INSTALLER_HEADER}) is used as relative path within 
+ * the bundle to search for installable  resources. Also files in sub folders 
+ * are considered as installable resources.<p>
+ * The files are installed in the order as returned by
+ * {@link Bundle#findEntries(String, String, boolean)}. Directories are 
+ * ignored.<p>
+ * All resources installed by this provider do use 
+ * {@link BundleInstallerConstants#PROVIDER_SCHEME} ({@value BundleInstallerConstants#PROVIDER_SCHEME}) as
+ * scheme and the path additional to the value of 
+ * {@link BundleInstallerConstants#BUNDLE_INSTALLER_HEADER}.<p>
+ * To give an example:<p>
+ * If the Bundle header notes<br>
+ * <pre><code>
+ *     {@value BundleInstallerConstants#BUNDLE_INSTALLER_HEADER}=resources
+ * </pre></code><br>
+ * and the bundle contains the resource <br>
+ * <pre><code>
+ *     resources/bundles/10/myBundle.jar
+ *     resources/config/myComponent.cfg
+ *     resoruces/data/myIndex.solrondex.zip
+ * </pre></code><br>
+ * than the following resources will be installed
+ * <pre><code>
+ *     {@value BundleInstallerConstants#PROVIDER_SCHEME}:bundles/10/myBundle.jar
+ *     {@value BundleInstallerConstants#PROVIDER_SCHEME}:config/myComponent.cfg
+ *     {@value BundleInstallerConstants#PROVIDER_SCHEME}:data/myIndex.solrondex.zip
+ * </pre></code><br>
+ * That means that {@link ResourceTransformer}s can both use the original name
+ * of the resource and the path relative to the install folder.
+ * <p>
+ * [1]  <a href="http://www.aqute.biz/Snippets/Extender"> The OSGi extender  pattern </a>
+ * <p>
+ * @author Rupert Westenthaler
+ *
+ */
+public class BundleInstaller implements BundleListener {
+
+    private static final Logger log = LoggerFactory.getLogger(BundleInstaller.class);
+    /** The scheme we use to register our resources. */
+
+    private final OsgiInstaller installer;
+    private final BundleContext context;
+    
+    /**
+     * contains all active bundles as key and the path to the config directory
+     * as value. A <code>null</code> value indicates that this bundle needs not
+     * to be processed. 
+     */
+    private final Map<Bundle,String> activated = new HashMap<Bundle,String>();
+
+    public BundleInstaller(OsgiInstaller installer,BundleContext context){
+        if(installer == null){
+            throw new IllegalArgumentException("The OsgiInstaller service MUST NOT be NULL");
+        }
+        if(context == null){
+            throw new IllegalArgumentException("The BundleContext MUST NOT be NULL");
+        }
+        this.installer = installer;
+        this.context = context;
+        this.context.addBundleListener(this);
+        //register the already active bundles
+        registerActive(this.context);
+    }
+    /**
+     * Uses the parsed bundle context to register the already active (and currently
+     * starting) bundles.
+     */
+    private void registerActive(BundleContext context) {
+        for (Bundle bundle : context.getBundles()){
+            if ((bundle.getState() & (Bundle.STARTING | Bundle.ACTIVE)) != 0) {
+                register(bundle);
+            }
+        }
+    }
+    @Override
+    public void bundleChanged(BundleEvent event) {
+        switch (event.getType()) {
+            case BundleEvent.STARTED:
+              register(event.getBundle());
+              break;
+              
+            case BundleEvent.STOPPED:
+              unregister(event.getBundle());
+              break;
+            
+            case BundleEvent.UPDATED:
+              unregister(event.getBundle());
+              register(event.getBundle());
+        }
+    }
+
+    /**
+     * Registers the bundle to the {@link #activated} map.
+     * @param bundle the bundle to register
+     */
+    @SuppressWarnings("unchecked")
+    private void register(Bundle bundle){
+        synchronized (activated) {
+            if(activated.containsKey(bundle)){
+                return;
+            } 
+        }
+        log.info("Register Bundle "+bundle.getSymbolicName()+" with BundleInstaller");
+        Dictionary<String,Object> headers = (Dictionary<String,Object>)bundle.getHeaders();
+//        log.info("With Headers:");
+//        for(Enumeration<String> keys = headers.keys();keys.hasMoreElements();){
+//            String key = keys.nextElement();
+//            log.info(" > "+key+"="+headers.get(key));
+//        }
+        String path = (String)headers.get(BUNDLE_INSTALLER_HEADER);
+        activated.put(bundle, path);
+        if(path != null){
+            log.info(" ... process configuration within path "+path);
+            ArrayList<InstallableResource> updated = new ArrayList<InstallableResource>();
+            for(Enumeration<URL> resources = (Enumeration<URL>)bundle.findEntries(path, null, true);resources.hasMoreElements();){
+                InstallableResource resource = createInstallableResource(bundle, path,resources.nextElement());
+                if(resource != null){
+                    updated.add(resource);
+                }
+            }
+            installer.updateResources(PROVIDER_SCHEME, updated.toArray(new InstallableResource[updated.size()]), new String[]{});
+        } else {
+            log.info("  ... no Configuration to process");
+        }
+    }
+    /**
+     * Creates an {@link InstallableResource} for {@link URL}s of files within
+     * the parsed bundle. 
+     * @param bundle the bundle containing the parsed resource
+     * @param bundleResource a resource within the bundle that need to be installed
+     * @return the installable resource or <code>null</code> in case of an error
+     */
+    private InstallableResource createInstallableResource(Bundle bundle, String path, URL bundleResource) {
+        //define the id
+        String id = bundleResource.toString();
+        String relPath = id.substring(id.lastIndexOf(path)+path.length(),id.length());
+        String name = FilenameUtils.getName(relPath);
+        if(name == null || name.isEmpty()){
+            return null; //ignore directories!
+        }
+        
+        InstallableResource resource;
+        try {
+            /*
+             * Notes:
+             *  - use bundleinstaller:<relativepath> as id
+             *  - parse null as type to enable autodetection for configs as
+             *    implemented by InternalReseouce.create(..)
+             *  - we use the modification date of the bundle as digest
+             *  - the Dictionary will be ignored if an input stream is present
+             *    so it is best to parse null
+             *  - No idea how the priority is used by the Sling Installer. For
+             *    now parse null than the default priority is used.
+             */
+            resource = new InstallableResource(
+                BundleInstallerConstants.PROVIDER_SCHEME+':'+relPath,
+                bundleResource.openStream(), null, 
+                String.valueOf(bundle.getLastModified()), null, null);
+            log.info(" ... found installable resource "+id);
+        } catch (IOException e) {
+            log.error(String.format("Unable to process configuration File %s from Bundle %s",
+                id,bundle.getSymbolicName()),e);
+            return null;
+        }
+        return resource;
+    }
+    private void unregister(Bundle bundle) {
+        String path;
+        synchronized (activated) {
+            if (!activated.containsKey(bundle)){
+                return;
+            }
+            path = activated.remove(bundle);
+        }
+        if(path != null) {
+            //remove the files ...
+            //TODO: Maybe removing installed stuff when the bundle is stopped is
+            //      not so a good Idea! Maybe it is ?!
+        }
+    }
+    /**
+     * removes the bundle listener
+     */
+    public void close(){
+        context.removeBundleListener(this);
+    }
+}
