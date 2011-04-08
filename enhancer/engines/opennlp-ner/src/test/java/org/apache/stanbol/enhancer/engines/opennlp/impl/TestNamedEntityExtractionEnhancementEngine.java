fa9e038449712fee8e89ff7cac95048b8f93fbbf
Olivier Grisel <ogrisel@apache.org>
2011-04-08 09:20:23 +0000

    better name for the Named Entity engine core
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1090165 13f79535-47bb-0310-9956-ffa450edef68

--- enhancer/engines/opennlp-ner/src/test/java/org/apache/stanbol/enhancer/engines/opennlp/impl/TestNamedEntityExtractionEnhancementEngine.java
+++ enhancer/engines/opennlp-ner/src/test/java/org/apache/stanbol/enhancer/engines/opennlp/impl/TestNamedEntityExtractionEnhancementEngine.java
@@ -57,13 +57,13 @@ public class TestNamedEntityExtractionEnhancementEngine extends Assert {
             + " without any name.\n"
             + "A new paragraph is being written. This paragraph has two sentences.";
 
-    static EngineCore nerEngine;
+    static NEREngineCore nerEngine;
     
     public static final String FAKE_BUNDLE_SYMBOLIC_NAME = "FAKE_BUNDLE_SYMBOLIC_NAME";
 
     @BeforeClass
     public static void setUpServices() throws IOException {
-        nerEngine = new EngineCore(new ClasspathDataFileProvider(FAKE_BUNDLE_SYMBOLIC_NAME), FAKE_BUNDLE_SYMBOLIC_NAME);
+        nerEngine = new NEREngineCore(new ClasspathDataFileProvider(FAKE_BUNDLE_SYMBOLIC_NAME), FAKE_BUNDLE_SYMBOLIC_NAME);
     }
 
     public static ContentItem wrapAsContentItem(final String id,
