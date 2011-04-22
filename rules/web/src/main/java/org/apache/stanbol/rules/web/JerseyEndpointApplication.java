eaf06a9df42f5c528fe0f66d91b376fec55e5d98
Andrea Giovanni Nuzzolese <anuzzolese@apache.org>
2011-04-22 11:43:28 +0000

    STANBOL-182 Added the missing imports for RefactorerResource, RestRecipe, RestRule and RuleStoreResource in JerseyEndpointApplication of rules.web.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1095922 13f79535-47bb-0310-9956-ffa450edef68

--- rules/web/src/main/java/org/apache/stanbol/rules/web/JerseyEndpointApplication.java
+++ rules/web/src/main/java/org/apache/stanbol/rules/web/JerseyEndpointApplication.java
@@ -8,6 +8,10 @@ import javax.ws.rs.core.Application;
 import org.apache.stanbol.commons.web.base.writers.GraphWriter;
 import org.apache.stanbol.commons.web.base.writers.ResultSetWriter;
 import org.apache.stanbol.owl.web.OWLOntologyWriter;
+import org.apache.stanbol.rules.web.resources.RefactorerResource;
+import org.apache.stanbol.rules.web.resources.RestRecipe;
+import org.apache.stanbol.rules.web.resources.RestRule;
+import org.apache.stanbol.rules.web.resources.RuleStoreResource;
 
 /**
  * Statically define the list of available resources and providers to be used by the KReS JAX-RS Endpoint.
