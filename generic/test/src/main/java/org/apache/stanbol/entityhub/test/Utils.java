16a6180b600069d048b3c142a48040e42d58b461
Olivier Grisel <ogrisel@apache.org>
2011-05-16 10:15:31 +0000

    format YardTest
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/entityhub@1103672 13f79535-47bb-0310-9956-ffa450edef68

--- generic/test/src/main/java/org/apache/stanbol/entityhub/test/Utils.java
+++ generic/test/src/main/java/org/apache/stanbol/entityhub/test/Utils.java
@@ -21,17 +21,19 @@ import java.util.Collection;
 import java.util.Iterator;
 
 public final class Utils {
-    private Utils(){}
+    private Utils() {}
+
     /**
-     * Converts an Iterator to a Collection by iterating over all elements and
-     * adding them to a List.
+     * Converts an Iterator to a Collection by iterating over all elements and adding them to a List.
+     * 
      * @param <T>
-     * @param it the iterator
+     * @param it
+     *            the iterator
      * @return the collection containing all elements of the iterator
      */
-    public static <T> Collection<T> asCollection(Iterator<T> it){
+    public static <T> Collection<T> asCollection(Iterator<T> it) {
         Collection<T> c = new ArrayList<T>();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             c.add(it.next());
         }
         return c;
