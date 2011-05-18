9e139ad2406fbe366d0ac48ca3a4b3de770a6da9
Suat Gonul <suat@apache.org>
2011-05-18 23:00:01 +0000

    STANBOL-206: -It is not allowed to define a standalone property bridge, so XSD schema was updated accordingly and model was regenerated.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1124460 13f79535-47bb-0310-9956-ffa450edef68

--- cmsadapter/servicesapi/src/main/java/org/apache/stanbol/cmsadapter/servicesapi/helper/MappingModelParser.java
+++ cmsadapter/servicesapi/src/main/java/org/apache/stanbol/cmsadapter/servicesapi/helper/MappingModelParser.java
@@ -14,99 +14,100 @@ import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.BridgeDefinitions
 import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.ConceptBridge;
 import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.InstanceBridge;
 import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.ObjectFactory;
-import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.PropertyBridge;
 import org.apache.stanbol.cmsadapter.servicesapi.model.mapping.SubsumptionBridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Helper class for serializing and deserializing bridge definitions
+ * 
+ */
 public class MappingModelParser {
-	private static final Logger logger = LoggerFactory
-			.getLogger(MappingModelParser.class);
+    private static final Logger logger = LoggerFactory.getLogger(MappingModelParser.class);
 
-	public static BridgeDefinitions deserializeObject(String xmlContent) {
-		BridgeDefinitions bridgeDefinitions = null;
+    public static BridgeDefinitions deserializeObject(String xmlContent) {
+        BridgeDefinitions bridgeDefinitions = null;
 
-		try {
-			ClassLoader cl = ObjectFactory.class.getClassLoader();
-			JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class
-					.getPackage().getName(), cl);
-			Unmarshaller um = jc.createUnmarshaller();
-			StringReader stringReader = new StringReader(xmlContent);
-			bridgeDefinitions = (BridgeDefinitions) um.unmarshal(stringReader);
+        try {
+            ClassLoader cl = ObjectFactory.class.getClassLoader();
+            JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName(), cl);
+            Unmarshaller um = jc.createUnmarshaller();
+            StringReader stringReader = new StringReader(xmlContent);
+            bridgeDefinitions = (BridgeDefinitions) um.unmarshal(stringReader);
 
-		} catch (JAXBException e) {
-			logger.error("JAXB Exception when parsing serialized BridgeDefinitions");
-		}
-		return bridgeDefinitions;
-	}
+        } catch (JAXBException e) {
+            logger.error("JAXB Exception when parsing serialized BridgeDefinitions");
+        }
+        return bridgeDefinitions;
+    }
 
-	   public static String serializeObject(Object object) {
-	        String bridgeDefinitions = null;
+    public static String serializeObject(Object object) {
+        String bridgeDefinitions = null;
 
-	        try {
-	            ClassLoader cl = ObjectFactory.class.getClassLoader();
-	            JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class
-	                    .getPackage().getName(), cl);
-	            Marshaller m = jc.createMarshaller();
-	            StringWriter stringWriter = new StringWriter();
-	            m.marshal(object, stringWriter);
-	            bridgeDefinitions = stringWriter.toString();
+        try {
+            ClassLoader cl = ObjectFactory.class.getClassLoader();
+            JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName(), cl);
+            Marshaller m = jc.createMarshaller();
+            StringWriter stringWriter = new StringWriter();
+            m.marshal(object, stringWriter);
+            bridgeDefinitions = stringWriter.toString();
 
-	        } catch (JAXBException e) {
-	            logger.error("JAXB Exception when parsing serialized BridgeDefinitions");
-	        }
-	        return bridgeDefinitions;
-	    }
+        } catch (JAXBException e) {
+            logger.error("JAXB Exception when parsing serialized BridgeDefinitions");
+        }
+        return bridgeDefinitions;
+    }
 
+    /**
+     * Gets {@link ConceptBridge}s from inside the given {@link BridgeDefinitions} object.
+     * 
+     * @param bridgeDefinitions
+     * @return
+     */
+    public static List<ConceptBridge> getConceptBridges(BridgeDefinitions bridgeDefinitions) {
+        List<ConceptBridge> cList = new ArrayList<ConceptBridge>();
+        List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrInstanceBridge();
+        for (Object bridge : aList) {
+            if (bridge instanceof ConceptBridge) {
+                cList.add((ConceptBridge) bridge);
+            }
+        }
+        return cList;
+    }
 
-	public static List<ConceptBridge> getConceptBridges(
-			BridgeDefinitions bridgeDefinitions) {
-		List<ConceptBridge> cList = new ArrayList<ConceptBridge>();
-		List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrPropertyBridge();
-		for (Object bridge : aList) {
-			if (bridge instanceof ConceptBridge) {
-				cList.add((ConceptBridge) bridge);
-			}
-		}
-		return cList;
-	}
+    /**
+     * Gets {@link SubsumptionBridge}s from inside the given {@link BridgeDefinitions} object.
+     * 
+     * @param bridgeDefinitions
+     * @return
+     */
+    public static List<SubsumptionBridge> getSubsumptionBridges(BridgeDefinitions bridgeDefinitions) {
 
-	public static List<SubsumptionBridge> getSubsumptionBridges(
-			BridgeDefinitions bridgeDefinitions) {
+        List<SubsumptionBridge> sList = new ArrayList<SubsumptionBridge>();
+        List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrInstanceBridge();
+        for (Object bridge : aList) {
+            if (bridge instanceof SubsumptionBridge) {
+                sList.add((SubsumptionBridge) bridge);
+            }
+        }
+        return sList;
+    }
 
-		List<SubsumptionBridge> sList = new ArrayList<SubsumptionBridge>();
-		List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrPropertyBridge();
-		for (Object bridge : aList) {
-			if (bridge instanceof SubsumptionBridge) {
-				sList.add((SubsumptionBridge) bridge);
-			}
-		}
-		return sList;
-	}
+    /**
+     * Gets {@link InstanceBridge}s from inside the given {@link BridgeDefinitions} object.
+     * 
+     * @param bridgeDefinitions
+     * @return
+     */
+    public static List<InstanceBridge> getInstanceBridges(BridgeDefinitions bridgeDefinitions) {
 
-	public static List<PropertyBridge> getPropertyBridges(
-			BridgeDefinitions bridgeDefinitions) {
-
-		List<PropertyBridge> pList = new ArrayList<PropertyBridge>();
-		List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrPropertyBridge();
-		for (Object bridge : aList) {
-			if (bridge instanceof PropertyBridge) {
-				pList.add((PropertyBridge) bridge);
-			}
-		}
-		return pList;
-	}
-
-	public static List<InstanceBridge> getInstanceBridges(
-			BridgeDefinitions bridgeDefinitions) {
-
-		List<InstanceBridge> sList = new ArrayList<InstanceBridge>();
-		List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrPropertyBridge();
-		for (Object bridge : aList) {
-			if (bridge instanceof InstanceBridge) {
-				sList.add((InstanceBridge) bridge);
-			}
-		}
-		return sList;
-	}
+        List<InstanceBridge> sList = new ArrayList<InstanceBridge>();
+        List<Object> aList = bridgeDefinitions.getConceptBridgeOrSubsumptionBridgeOrInstanceBridge();
+        for (Object bridge : aList) {
+            if (bridge instanceof InstanceBridge) {
+                sList.add((InstanceBridge) bridge);
+            }
+        }
+        return sList;
+    }
 }
