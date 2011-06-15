11a72afaf91ae103dbf0e0ced22d9927ab656907
Fabian Christ <fchrist@apache.org>
2011-06-15 07:26:25 +0000

    STANBOL-227 Branch from /enhancer/generic/jsonld
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1135944 13f79535-47bb-0310-9956-ffa450edef68

---
+++ commons/jsonld/src/main/java/org/apache/stanbol/jsonld/JsonLdIRI.java
@@ -0,0 +1,15 @@
+package org.apache.stanbol.jsonld;
+
+public class JsonLdIRI {
+
+    private final String IRI;
+
+    public JsonLdIRI(String IRI) {
+        this.IRI = IRI;
+    }
+    
+    public String getIRI() {
+        return IRI;
+    }
+
+}
