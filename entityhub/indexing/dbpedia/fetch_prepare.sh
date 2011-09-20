202e81c21e27062c40064e37c93cefbf36fbe266
Olivier Grisel <ogrisel@apache.org>
2011-09-20 16:49:00 +0000

    STANBOL-323: clean images_en.nt file manually before importing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1173246 13f79535-47bb-0310-9956-ffa450edef68

--- entityhub/indexing/dbpedia/fetch_prepare.sh
+++ entityhub/indexing/dbpedia/fetch_prepare.sh
@@ -45,8 +45,15 @@ wget -c $DBPEDIA/ru/labels_ru.nt.bz2
 wget -c $DBPEDIA/tr/labels_tr.nt.bz2
 wget -c $DBPEDIA/zh/labels_zh.nt.bz2
 wget -c $DBPEDIA/en/short_abstracts_en.nt.bz2
-wget -c $DBPEDIA/en/images_en.nt.bz2
 #wget -c $DBPEDIA/en/long_abstracts_en.not.bz2
+
+# special handling of the image file that has 5 corrupted entries
+if [ ! -f images_en.nt ]
+then
+    wget -c $DBPEDIA/en/images_en.nt.bz2
+    bzcat images_en.nt.bz2 | grep -v '\\' > images_en.nt
+    rm -f images_en.nt.bz2
+fi
 
 # Type specific attributes
 wget -c $DBPEDIA/en/geo_coordinates_en.nt.bz2
