19663851e74a8d82a9ecfa6b10cf690a5c680eb6
Olivier Grisel <ogrisel@apache.org>
2011-09-21 14:53:36 +0000

    cosmit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/entityhub@1173691 13f79535-47bb-0310-9956-ffa450edef68

--- indexing/core/src/main/java/org/apache/stanbol/entityhub/indexing/core/source/ResourceLoader.java
+++ indexing/core/src/main/java/org/apache/stanbol/entityhub/indexing/core/source/ResourceLoader.java
@@ -203,13 +203,13 @@ public class ResourceLoader {
                 //during init it is checked that files exists and are files 
                 //and there is read access so this can only happen if
                 //someone deletes the file in between
-                log.error("Unable to load resource "+file+"!",e);
+                log.warn("Unable to load resource " + file, e);
                 setResourceState(file, ResourceState.ERROR, e);
             } catch (IOException e) {
-                log.error("Unable to load resource "+file+"!",e);
+                log.error("Unable to load resource " + file ,e);
                 setResourceState(file, ResourceState.ERROR, e);
             } catch (Exception e) {
-                log.error("Unable to load resource "+file+"!",e);
+                log.error("Unable to load resource " + file, e);
                 setResourceState(file, ResourceState.ERROR, e);
             }
         }
