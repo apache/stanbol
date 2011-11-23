9abac3c714cf9502a8e96ec302c78b615dd09fe0
Enrico Daga <enridaga@apache.org>
2011-11-23 10:09:04 +0000

    Moved the jobs modules to /trunk/commons/jobs, deleting it from reasoners (STANBOL-343, STANBOL-383)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/branches/lto-reasoners@1205354 13f79535-47bb-0310-9956-ffa450edef68

--- reasoners/jobs/api/src/main/java/org/apache/stanbol/commons/jobs/api/Job.java
+++
@@ -1,8 +0,0 @@
-package org.apache.stanbol.commons.jobs.api;
-
-import java.util.concurrent.Callable;
-
-public interface Job extends Callable<JobResult> {
-    
-    public String buildResultLocation(String jobId);
-}
