a7b29c0172fe9365b23428c1cfda1a03b557c35c
Enrico Daga <enridaga@apache.org>
2011-11-23 16:00:32 +0000

    Added ASF License headers to java classes (STANBOL-343)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/branches/lto-reasoners@1205466 13f79535-47bb-0310-9956-ffa450edef68

--- reasoners/web/src/main/java/org/apache/stanbol/reasoners/web/resources/ReasoningServiceTaskResource.java
+++ reasoners/web/src/main/java/org/apache/stanbol/reasoners/web/resources/ReasoningServiceTaskResource.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.stanbol.reasoners.web.resources;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
@@ -72,6 +88,8 @@ import com.sun.jersey.multipart.FormDataMultiPart;
  * 
  * This class includes methods to prepare the input and dispatch the output (back to the client in the
  * requested format or saved in the triple store).
+ * 
+ * Support for long term operations is provided by adding /job to the request URI
  * 
  */
 @Path("/reasoners/{service}/{task: [^/]+}{job: (/job)?}")
