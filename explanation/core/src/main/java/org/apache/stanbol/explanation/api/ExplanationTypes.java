4334b68b6390626fa4be136ce165bc17e0f7cae9
Alessandro Adamou <alexdma@apache.org>
2012-02-03 17:59:24 +0000

    STANBOL-347 : explanation moved out of trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1240265 13f79535-47bb-0310-9956-ffa450edef68

--- explanation/core/src/main/java/org/apache/stanbol/explanation/api/ExplanationTypes.java
+++
@@ -1,8 +0,0 @@
-package org.apache.stanbol.explanation.api;
-
-public enum ExplanationTypes {
-
-    KNOWLEDGE_OBJECT_SYNOPSIS,
-    UI_FEEDBACK_JUSTIFICATION;
-
-}
--- explanation/core/src/main/java/org/apache/stanbol/explanation/heuristics/IDTypes.java
+++
@@ -1,7 +0,0 @@
-package org.apache.stanbol.explanation.heuristics;
-
-public enum IDTypes {
-    CMS_USERNAME,
-    FOAF_ID,
-    SSN;
-}
