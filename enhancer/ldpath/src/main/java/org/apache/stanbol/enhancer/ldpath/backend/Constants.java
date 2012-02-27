a563c9cada5f97c86eb66eeea01078516cdbd755
Rupert Westenthaler <rwesten@apache.org>
2012-02-27 11:18:22 +0000

    STANBOL-500:  * removed two unused classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1294113 13f79535-47bb-0310-9956-ffa450edef68

--- enhancer/ldpath/src/main/java/org/apache/stanbol/enhancer/ldpath/backend/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.apache.stanbol.enhancer.ldpath.backend;
-
-public final class Constants {
-
-    private Constants(){}
-    
-    private static final String CI_NAMESPACE = "urn:apache.stanbol.enhancer:ldpath.property:";
-
-}
