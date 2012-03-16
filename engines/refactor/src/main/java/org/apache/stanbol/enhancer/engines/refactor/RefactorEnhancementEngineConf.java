d1df1cf25404c3f44e3dce2fe310bd1e388f64c9
Alessandro Adamou <alexdma@apache.org>
2012-03-16 13:34:23 +0000

    This commit applies the following patches proposed by Alberto Musetti: * Add CORS support to Reasoners, Rules and Reengineer (STANBOL-105) * Replaced most stdout prints in Rules with SLF4j (STANBOL-253) * Introduces default (inactive) SEO configuration for refactor engine and fixes two bugs for boolean conversion and graph clearing (STANBOL-468)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/enhancer@1301497 13f79535-47bb-0310-9956-ffa450edef68

--- engines/refactor/src/main/java/org/apache/stanbol/enhancer/engines/refactor/RefactorEnhancementEngineConf.java
+++ engines/refactor/src/main/java/org/apache/stanbol/enhancer/engines/refactor/RefactorEnhancementEngineConf.java
@@ -17,10 +17,8 @@
 
 package org.apache.stanbol.enhancer.engines.refactor;
 
-import org.apache.felix.scr.annotations.Property;
-
 /**
- *
+ * 
  * @author anuzzolese
  * @author alberto.musetti
  * 
@@ -36,58 +34,55 @@ public interface RefactorEnhancementEngineConf {
      * The OntoNet scope that the engine should use.
      */
     String SCOPE = "org.apache.stanbol.enhancer.engines.refactor.scope";
-    
+
     /**
      * The location from which the recipe is loaded.
      */
     String RECIPE_LOCATION = "org.apache.stanbol.enhancer.engines.refactor.recipe.location";
-    
+
     /**
      * The ID used for identifying the recipe in the RuleStore.
      */
     String RECIPE_ID = "org.apache.stanbol.enhancer.engines.refactor.recipe.id";
-    
+
     /**
      * The set of ontology URIs that should be loaded in the core space of the scope.
      */
     String SCOPE_CORE_ONTOLOGY = "org.apache.stanbol.enhancer.engines.refactor.scope.core.ontology";
-    
+
     /**
-     * If true: the previously generated RDF is deleted and substituted with the new one. 
-     * If false: the new one is appended to the old RDF. 
-     * Possible value in the configuration: true or false.
+     * If true: the previously generated RDF is deleted and substituted with the new one. If false: the new
+     * one is appended to the old RDF. Possible value in the configuration: true or false.
      */
     String APPEND_OTHER_ENHANCEMENT_GRAPHS = "org.apache.stanbol.enhancer.engines.refactor.append.graphs";
-    
+
     /**
-     * If true: entities are fetched via the EntityHub. 
-     * If false: entities are fetched on-line. 
-     * Possible value in the configuration: true or false.
+     * If true: entities are fetched via the EntityHub. If false: entities are fetched on-line. Possible value
+     * in the configuration: true or false.
      */
-    String USE_ENTITY_HUB  = "org.apache.stanbol.enhancer.engines.refactor.entityhub";
-    
-    
+    String USE_ENTITY_HUB = "org.apache.stanbol.enhancer.engines.refactor.entityhub";
+
     public String getScope();
-    
+
     public void setScope(String scopeId);
-    
+
     public String getRecipeLocation();
-    
+
     public void setRecipeLocation(String recipeLocation);
-    
+
     public String getRecipeId();
-    
+
     public void setRecipeId(String recipeId);
-    
+
     public String[] getScopeCoreOntology();
-    
+
     public void setScopeCoreOntology(String[] coreOntologyIRI);
-    
+
     public boolean isInGraphAppendMode();
-    
+
     public void setInGraphAppendMode(boolean b);
-    
+
     public boolean isEntityHubUsed();
-    
+
     public void setEntityHubUsed(boolean b);
 }
