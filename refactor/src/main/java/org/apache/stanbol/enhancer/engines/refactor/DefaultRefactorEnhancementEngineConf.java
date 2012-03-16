ffeb146d625ee2ae816e8bbe2eeb394c6b44d8e1
Alessandro Adamou <alexdma@apache.org>
2012-03-16 13:34:23 +0000

    This commit applies the following patches proposed by Alberto Musetti: * Add CORS support to Reasoners, Rules and Reengineer (STANBOL-105) * Replaced most stdout prints in Rules with SLF4j (STANBOL-253) * Introduces default (inactive) SEO configuration for refactor engine and fixes two bugs for boolean conversion and graph clearing (STANBOL-468)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/enhancer/engines@1301497 13f79535-47bb-0310-9956-ffa450edef68

--- refactor/src/main/java/org/apache/stanbol/enhancer/engines/refactor/DefaultRefactorEnhancementEngineConf.java
+++ refactor/src/main/java/org/apache/stanbol/enhancer/engines/refactor/DefaultRefactorEnhancementEngineConf.java
@@ -14,96 +14,93 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.stanbol.enhancer.engines.refactor;
 
 import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Default configuration of the Refactor Enhancement Engine.<br/>
- * The default configuration load an instance of Refactor Engine which maps named-entities recognized by
- * other Enhancement Engines to Schema.org.
- *  
+ * The default configuration load an instance of Refactor Engine which maps named-entities recognized by other
+ * Enhancement Engines to Schema.org.
+ * 
  * @author anuzzolese
  * @author alberto.musetti
- *
+ * 
  */
 public class DefaultRefactorEnhancementEngineConf implements RefactorEnhancementEngineConf {
 
-	private Dictionary<String, Object> conf;
-	
-	/*
-	public DefaultRefactorEnhancementEngineConf() {
-		
-	}
-	*/
-	public DefaultRefactorEnhancementEngineConf(Dictionary<String, Object> map) {
-		this.conf = map;
-	}
-	
-	@Override
-	public String getScope() {
-		return (String) conf.get(SCOPE);
-	}
+    private Dictionary<String,Object> conf;
 
-	@Override
-	public void setScope(String scopeId) {
-		conf.put(SCOPE, scopeId);
-	}
+    /*
+     * public DefaultRefactorEnhancementEngineConf() {
+     * 
+     * }
+     */
+    public DefaultRefactorEnhancementEngineConf(Dictionary<String,Object> map) {
+        this.conf = map;
+    }
 
-	@Override
-	public String getRecipeLocation() {
-		return (String) conf.get(RECIPE_LOCATION);
-	}
+    @Override
+    public String getScope() {
+        return (String) conf.get(SCOPE);
+    }
 
-	@Override
-	public void setRecipeLocation(String recipeLocation) {
-		conf.put(RECIPE_LOCATION, recipeLocation);
-		
-	}
+    @Override
+    public void setScope(String scopeId) {
+        conf.put(SCOPE, scopeId);
+    }
 
-	@Override
-	public String getRecipeId() {
-		return (String) conf.get(RECIPE_ID);
-	}
+    @Override
+    public String getRecipeLocation() {
+        return (String) conf.get(RECIPE_LOCATION);
+    }
 
-	@Override
-	public void setRecipeId(String recipeId) {
-		conf.put(RECIPE_ID, recipeId);
-	}
+    @Override
+    public void setRecipeLocation(String recipeLocation) {
+        conf.put(RECIPE_LOCATION, recipeLocation);
 
-	@Override
-	public String[] getScopeCoreOntology() {
-		return (String[]) conf.get(SCOPE_CORE_ONTOLOGY);
-	}
+    }
 
-	@Override
-	public void setScopeCoreOntology(String[] coreOntologyURI) {
-		conf.put(SCOPE_CORE_ONTOLOGY, coreOntologyURI);
-	}
+    @Override
+    public String getRecipeId() {
+        return (String) conf.get(RECIPE_ID);
+    }
 
-	@Override
-	public boolean isInGraphAppendMode() {
-		return ((Boolean) conf.get(APPEND_OTHER_ENHANCEMENT_GRAPHS)).booleanValue();
-	}
+    @Override
+    public void setRecipeId(String recipeId) {
+        conf.put(RECIPE_ID, recipeId);
+    }
 
-	@Override
-	public void setInGraphAppendMode(boolean b) {
-		conf.put(APPEND_OTHER_ENHANCEMENT_GRAPHS, b);
-		
-	}
+    @Override
+    public String[] getScopeCoreOntology() {
+        return (String[]) conf.get(SCOPE_CORE_ONTOLOGY);
+    }
 
-	@Override
-	public boolean isEntityHubUsed() {
-		return ((Boolean) conf.get(USE_ENTITY_HUB)).booleanValue();
-	}
+    @Override
+    public void setScopeCoreOntology(String[] coreOntologyURI) {
+        conf.put(SCOPE_CORE_ONTOLOGY, coreOntologyURI);
+    }
 
-	@Override
-	public void setEntityHubUsed(boolean b) {
-		conf.put(USE_ENTITY_HUB, b);
-		
-	}
+    @Override
+    public boolean isInGraphAppendMode() {
+        return Boolean.valueOf(conf.get(APPEND_OTHER_ENHANCEMENT_GRAPHS).toString()).booleanValue();
+    }
+
+    @Override
+    public void setInGraphAppendMode(boolean b) {
+        conf.put(APPEND_OTHER_ENHANCEMENT_GRAPHS, b);
+
+    }
+
+    @Override
+    public boolean isEntityHubUsed() {
+        return Boolean.valueOf(conf.get(USE_ENTITY_HUB).toString()).booleanValue();
+    }
+
+    @Override
+    public void setEntityHubUsed(boolean b) {
+        conf.put(USE_ENTITY_HUB, b);
+
+    }
 
 }
