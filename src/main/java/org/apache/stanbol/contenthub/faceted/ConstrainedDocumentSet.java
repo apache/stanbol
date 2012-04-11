dfb552d764525bf56494d5ab1cfe5a4c9322a386
Reto Bachmann-Gmür <reto@apache.org>
2012-04-11 08:58:25 +0000

    A draft for a facade to faceted search
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/branches/faceted-browsing@1324653 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/stanbol/contenthub/faceted/ConstrainedDocumentSet.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2012 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.stanbol.contenthub.faceted;
+
+import java.util.Locale;
+import java.util.Set;
+import org.apache.clerezza.rdf.core.Resource;
+import org.apache.clerezza.rdf.core.UriRef;
+
+/**
+ * A set of documents that can be narrowed by applying constraints that are 
+ * grouped by facets. Instances of this class are immutable, narrowing and 
+ * broadening return new instances.
+ */
+public interface ConstrainedDocumentSet {
+	
+	/**
+	 * A constraint requires a document property to have a certain value
+	 */
+	//TODO two versions of Constraint one with value (like this one) and the other with a value range
+	interface Constraint {
+		/**
+		 * 
+		 * @return the facet this constraint relates too
+		 */
+		Facet getFacet();
+		
+		/**
+		 * 
+		 * @return the URIRef of Literal that documents matching this constraint have as value of this facet
+		 */
+		Resource getValue();
+	}
+	
+	/**
+	 * A facet (aspect) by which the ConstrainedDocumentSet can be narrowed
+	 */
+	interface Facet {
+		
+		/**
+		 * 
+		 * @return a set of constraints that reduce the document to a non-empty set
+		 */
+		Set<Constraint> getConstraints();
+		
+		/**
+		 * 
+		 * @return The property this facet maps too
+		 */
+		UriRef getProperty();
+		
+		/**
+		 * 
+		 * @param locale the desired locale or null if no preference
+		 * @return a label for this facet
+		 */
+		String getLabel(Locale locale);
+		
+	}
+	
+	
+	/**
+	 * Note that this method might be implemented so that getting the size of
+	 * the set doesn't require enumerating the documents
+	 * 
+	 * @return the documents matching this ConstrainedDocumentSet
+	 */
+	//TODO is just retuning the UriRef enough of should we rettur a DocumentResult or use ContentItem
+	Set<UriRef> getDocuments();
+	
+	/**
+	 * This is the breadcrumb of this ConstrainedDocumentSet
+	 * 
+	 * @return the constrains that apply to this ConstrainedDocumentSet (might be empty)
+	 */
+	Set<Constraint> getConstraints();
+	
+	/**
+	 * 
+	 * @return the facets by which this ConstrainedDocumentSet can be restricted
+	 */
+	Set<Facet> getFacets();
+	
+	/**
+	 * Note that the new set need not to be computed when this method is called,
+	 * the matching document might be computed when needed. So implimentations
+	 * can provided efficient way to allow a client to call
+	 * 
+	 * <code>narrow(additionalConstraint).getDocuments().size()</code>
+	 * 
+	 * @param constraint the additional constraint to apply
+	 * @return the ConstrainedDocumentSet with that additional constraint
+	 */
+	ConstrainedDocumentSet narrow(Constraint constraint);
+	
+	/**
+	 * Removes a constraint
+	 * 
+	 * @param constraint the constraint which must be be member of the set returned by getConstraints()
+	 * @return the broadened ConstrainedDocumentSet
+	 */
+	ConstrainedDocumentSet broaden(Constraint constraint);
+	
+}
