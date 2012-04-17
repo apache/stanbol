d839716f9a38537259c5e745d01ac039b6620e0e
Fabian Christ <fchrist@apache.org>
2012-04-17 17:45:54 +0000

    STANBOL-587 Remove files from branch that are not part of the release archive
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/branches/0.9.0-incubating@1327201 13f79535-47bb-0310-9956-ffa450edef68

--- enhancer/SemiAutomaticContentEnhancer/README.txt
+++
@@ -1,6 +0,0 @@
-Semi-automatic content enhancer client for Stanbol Enhancer.
-
-To run this, build with "mvn clean install" and execute the resulting
-jar file:
-
-  java -jar target/org.apache.stanbol.clients.sace-*-jar-with-dependencies.jar
--- enhancer/query-rendering/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CP=bin:resource:`echo resource/am/*.jar | tr " " ":"`:`echo lib/*.jar | tr " " ":"`
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home/
-
-ant clean && ant && \
-echo ${CP} && ${JAVA_HOME}/bin/java -cp ${CP} -Xmx1g org.apache.stanbol.enhancer.interaction.Start 
