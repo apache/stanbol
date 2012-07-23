b56fd48794f3b164bd060d56623b048c7b5327e3
Rupert Westenthaler <rwesten@apache.org>
2012-07-23 06:51:06 +0000

    fixes STANBOL-698 by catching RuntimeExceptions while converting TypedLiterals to Java Objects and using the lexical form (String value) in case of an Exception.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk/entityhub@1364515 13f79535-47bb-0310-9956-ffa450edef68

--- model/clerezza/src/main/java/org/apache/stanbol/entityhub/model/clerezza/impl/Resource2ValueAdapter.java
+++ model/clerezza/src/main/java/org/apache/stanbol/entityhub/model/clerezza/impl/Resource2ValueAdapter.java
@@ -31,16 +31,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Converts the Resources (used to store field values in the Clerezza triple
- * store) back to values as defined by the {@link Representation} interface
+ * Converts the Resources (used to store field values in the Clerezza triple store) back to values as defined
+ * by the {@link Representation} interface
+ * 
  * @author Rupert Westenthaler
- *
- * @param <T> the type of the Resource that can be converted to values
+ * 
+ * @param <T>
+ *            the type of the Resource that can be converted to values
  */
-public class Resource2ValueAdapter<T extends Resource> implements Adapter<T, Object> {
+public class Resource2ValueAdapter<T extends Resource> implements Adapter<T,Object> {
 
     private static Logger log = LoggerFactory.getLogger(Resource2ValueAdapter.class);
-
 
     private final LiteralFactory literalFactory = LiteralFactory.getInstance();
 
@@ -48,42 +49,55 @@ public class Resource2ValueAdapter<T extends Resource> implements Adapter<T, Obj
 
     @Override
     public final Object adapt(T value, Class<Object> type) {
-        if(value instanceof UriRef){
-            return valueFactory.createReference((UriRef)value);
-        } else if(value instanceof PlainLiteral){
-            return valueFactory.createText((Literal)value);
-        } else if(value instanceof TypedLiteral){
+        if (value instanceof UriRef) {
+            return valueFactory.createReference((UriRef) value);
+        } else if (value instanceof PlainLiteral) {
+            return valueFactory.createText((Literal) value);
+        } else if (value instanceof TypedLiteral) {
             TypedLiteral literal = (TypedLiteral) value;
-            if(literal.getDataType() == null){ //if no dataType is defined
-                //return a Text without a language
+            if (literal.getDataType() == null) { // if no dataType is defined
+                // return a Text without a language
                 return valueFactory.createText(literal);
             } else {
                 XsdDataTypeEnum mapping = RdfResourceUtils.XSD_DATATYPE_VALUE_MAPPING.get(literal.getDataType());
-                if(mapping != null){
-                    if(mapping.getMappedClass() != null){
-                        return literalFactory.createObject(mapping.getMappedClass(), literal);
-                    } else { //if no mapped class
-                        //bypass the LiteralFactory and return the string representation
+                if (mapping != null) {
+                    if (mapping.getMappedClass() != null) {
+                        try {
+                            return literalFactory.createObject(mapping.getMappedClass(), literal);
+                        } catch (RuntimeException e){
+                            log.info("Unable to convert Literal value {} to Java Class {} because of {} with message {}",
+                                new Object[]{literal,mapping.getMappedClass().getSimpleName(),
+                                             e.getClass().getSimpleName(),e.getMessage()});
+                            log.trace("Exception:",e);
+                            //STANBOL-698: Decide what to do in such cases
+                            //(a) throw an exception
+                            // throw e;
+                            //(b) ignore illegal values
+                            //return null;
+                            //(c) use the lexical form
+                            return literal.getLexicalForm();
+                        }
+                    } else { // if no mapped class
+                        // bypass the LiteralFactory and return the string
+                        // representation
                         return literal.getLexicalForm();
                     }
-                } else { //if dataType is not supported
+                } else { // if dataType is not supported
                     /*
-                     * this could indicate two things:
-                     * 1) the SimpleLiteralFactory supports a new DataType and
-                     *    because of that it creates Literals with this Type
-                     * 2) Literals with this data type where created by other
-                     *    applications.
-                     * In the first case one need to update the enumeration. In
-                     * the second case using the LexicalForm should be OK
-                     * Rupert Westenthaler 2010.10.28
+                     * this could indicate two things: 1) the SimpleLiteralFactory supports a new DataType and
+                     * because of that it creates Literals with this Type 2) Literals with this data type
+                     * where created by other applications. In the first case one need to update the
+                     * enumeration. In the second case using the LexicalForm should be OK Rupert Westenthaler
+                     * 2010.10.28
                      */
-                    log.warn("Missing Mapping for DataType "+literal.getDataType().getUnicodeString()
-                            +" -> return String representation");
+                    log.warn("Missing Mapping for DataType {} -> return String representation",
+                        literal.getDataType().getUnicodeString());
                     return literal.getLexicalForm();
                 }
             }
         } else {
-            log.warn("Unsupported Resource Type "+value.getClass()+" -> return String by using the toString method");
+            log.warn("Unsupported Resource Type {} -> return String by using the toString method",
+                value.getClass());
             return value.toString();
         }
     }
