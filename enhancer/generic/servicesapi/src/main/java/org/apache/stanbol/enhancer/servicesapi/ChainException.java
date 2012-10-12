35060e2621ce63192249641407dd0ca11604d4c0
Rupert Westenthaler <rwesten@apache.org>
2012-10-12 15:17:32 +0000

    minor: removed unused Constructor that added a dependency to the servicesapi.helper package
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1397612 13f79535-47bb-0310-9956-ffa450edef68

--- enhancer/generic/servicesapi/src/main/java/org/apache/stanbol/enhancer/servicesapi/ChainException.java
+++ enhancer/generic/servicesapi/src/main/java/org/apache/stanbol/enhancer/servicesapi/ChainException.java
@@ -16,12 +16,12 @@
 */
 package org.apache.stanbol.enhancer.servicesapi;
 
-import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.getDependend;
-import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.getEngine;
-import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.isOptional;
-
-import org.apache.clerezza.rdf.core.Graph;
-import org.apache.clerezza.rdf.core.NonLiteral;
+//import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.getDependend;
+//import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.getEngine;
+//import static org.apache.stanbol.enhancer.servicesapi.helper.ExecutionPlanHelper.isOptional;
+//
+//import org.apache.clerezza.rdf.core.Graph;
+//import org.apache.clerezza.rdf.core.NonLiteral;
 
 /**
  * BaseException thrown by {@link Chain} implementations or
@@ -40,18 +40,20 @@ public class ChainException extends EnhancementException {
     public ChainException(String message, Throwable cause) {
         super(message,cause);
     }
-    /**
-     * Creates a chain exception for the parsed node within the parsed executionPlan
-     * @param executionPlan
-     * @param node
-     * @param message
-     * @param cause
-     */
-    public ChainException(Graph executionPlan, NonLiteral node, String message, Throwable cause){
-        super(String.format("Unable to execute node {} (engine: {} | optional : {}" +
-        		" | dependsOn : {}) because of: {}",
-            node,getEngine(executionPlan, node),
-            isOptional(executionPlan, node), getDependend(executionPlan, node),
-            message == null || message.isEmpty() ? "<unknown>": message),cause);
-    }
+    
+//Removed - unused
+//    /**
+//     * Creates a chain exception for the parsed node within the parsed executionPlan
+//     * @param executionPlan
+//     * @param node
+//     * @param message
+//     * @param cause
+//     */
+//    public ChainException(Graph executionPlan, NonLiteral node, String message, Throwable cause){
+//        super(String.format("Unable to execute node {} (engine: {} | optional : {}" +
+//        		" | dependsOn : {}) because of: {}",
+//            node,getEngine(executionPlan, node),
+//            isOptional(executionPlan, node), getDependend(executionPlan, node),
+//            message == null || message.isEmpty() ? "<unknown>": message),cause);
+//    }
 }
