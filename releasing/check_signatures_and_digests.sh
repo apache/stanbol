4da5dfb2ed5c698856d2b017daf12538852219d8
Fabian Christ <fchrist@apache.org>
2013-01-08 20:19:24 +0000

    Add shell script to check signatures and digests of source-release ZIPs in a directory
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1430484 13f79535-47bb-0310-9956-ffa450edef68

---
+++ releasing/check_signatures_and_digests.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# Checks signatures and digests of source-releases.
+#
+# Usage: check_signatures_and_digests.sh <DIR_WITH_SOURCE_RELEASE.ZIPs>
+#
+
+echo "################################################################################"
+echo "                          CHECK SIGNATURES AND DIGESTS                          "
+echo "################################################################################"
+
+for i in `find "$1" -type f | grep -v '\.\(asc\|sha1\|md5\)$'`
+do
+ f=`echo $i | sed 's/\.asc$//'`
+ echo "$f"
+ gpg --verify $f.asc 2>/dev/null
+ if [ "$?" = "0" ]; then CHKSUM="GOOD"; else CHKSUM="BAD!!!!!!!!"; fi
+ if [ ! -f "$f.asc" ]; then CHKSUM="----"; fi
+ echo "gpg:  ${CHKSUM}"
+
+ for tp in md5 sha1
+ do
+   if [ ! -f "$f.$tp" ]
+   then
+     CHKSUM="----"
+   else
+     A="`cat $f.$tp 2>/dev/null`"
+     B="`openssl $tp < $f 2>/dev/null | sed 's/.*= *//' `"
+     if [ "$A" = "$B" ]; then CHKSUM="GOOD (`cat $f.$tp`)"; else CHKSUM="BAD!! : $A not equal to $B"; fi
+   fi
+   echo "$tp : ${CHKSUM}"
+ done
+
+done
+
+if [ -z "${CHKSUM}" ]; then echo "WARNING: no files found!"; fi
+
+echo "################################################################################"
