ac4db0d39b3d73d84bb0863542ebc98c13f4baaf
Rupert Westenthaler <rwesten@apache.org>
2013-02-28 11:49:15 +0000

    STANBOL-760: Major update to the Sentiment Summarization Engine: Engine supports not the detection of Phrases. Sentiments within phrases are combined using the Probabilistic sum := a+b-(a*b) ensuring that multiple positives sentiments appled to the same noun rise the sentiment value (instead of using the mean). The mean is still used when combining multiple sentiment phrases e.g. for the sentiment of a whole sentence or the document as a whole.
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk/enhancement-engines@1451179 13f79535-47bb-0310-9956-ffa450edef68

--- sentiment-summarization/src/main/java/org/apache/stanbol/enhancer/engines/sentiment/summarize/Sentiment.java
+++ sentiment-summarization/src/main/java/org/apache/stanbol/enhancer/engines/sentiment/summarize/Sentiment.java
@@ -2,9 +2,18 @@ package org.apache.stanbol.enhancer.engines.sentiment.summarize;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.stanbol.enhancer.nlp.NlpAnnotations;
+import org.apache.stanbol.enhancer.nlp.model.AnalysedText;
+import org.apache.stanbol.enhancer.nlp.model.Section;
+import org.apache.stanbol.enhancer.nlp.model.Sentence;
 import org.apache.stanbol.enhancer.nlp.model.Token;
+import org.apache.stanbol.enhancer.nlp.model.annotation.Value;
+import org.apache.stanbol.enhancer.nlp.pos.LexicalCategory;
+import org.apache.stanbol.enhancer.nlp.pos.PosTag;
 
 /**
  * This class is used to allow adding negations to sentiments even if the
@@ -17,14 +26,59 @@ import org.apache.stanbol.enhancer.nlp.model.Token;
  */
 public class Sentiment {
     
-    private Token token;
-    private double value;
+    /**
+     * Minimum POS tag confidence so that the annotated POS is used without
+     * considering the {@link #PREF_LEX_CAT}
+     */
+    private static final double MIN_POS_CONF = 0.85;
+    /**
+     * if the confidence of the main POS tag is lower then {@link #MIN_POS_CONF},
+     * than all POS tags are searched for the an POS annotation compatible with
+     * {@link #PREF_LEX_CAT}.
+     */
+    private static final Set<LexicalCategory> PREF_LEX_CAT = EnumSet.of(LexicalCategory.Adjective);
+    
+    private final Token token;
+    private final double value;
+    private final Sentence sentence;
     private List<Token> negated;
-    private List<Token> nouns;
+    private List<Token> aboutness;
+    private PosTag posTag;
 
-    public Sentiment(Token token, double value) {
+    private int start;
+    private int end;
+    private Token verb;
+    
+    /**
+     * The Token with the sentiment, the value of the sentiment and optionally
+     * the Sentence for the token
+     * @param token
+     * @param value
+     * @param sentence
+     */
+    public Sentiment(Token token, double value, Sentence sentence) {
         this.token = token;
         this.value = value;
+        this.sentence = sentence;
+        this.start = token.getStart();
+        this.end = token.getEnd();
+        List<Value<PosTag>> tags = token.getAnnotations(NlpAnnotations.POS_ANNOTATION);
+        for(Value<PosTag> tag : tags){
+            if(tag.probability() >= MIN_POS_CONF || 
+                    !Collections.disjoint(tag.value().getCategories(),PREF_LEX_CAT)){
+                posTag = tag.value();
+                break;
+            }
+        }
+        if(posTag == null){
+            posTag = tags.get(0).value();
+        }
+        if(posTag.hasCategory(LexicalCategory.Noun)){
+            addAbout(token); //add the token also as noun
+        }
+        if(posTag.hasCategory(LexicalCategory.Verb)){
+            setVerb(token);
+        }
     }
     
     public void negate(Token token){
@@ -36,8 +90,41 @@ public class Sentiment {
             l.add(token);
             negated = l;
         }
+        checkSpan(token);
     }
-    
+    protected final void setVerb(Token verb) {
+        this.verb = verb;
+        checkSpan(verb);
+    }
+
+    protected final void addAbout(Token noun) {
+        if(aboutness == null){
+            aboutness = new ArrayList<Token>(4);
+        }
+        aboutness.add(noun);
+        checkSpan(noun);
+    }
+    /**
+     * Checks the {@link #start} {@link #end} values against the span selected
+     * by the parsed token
+     * @param token
+     */
+    private void checkSpan(Token token) {
+        if(start > token.getStart()){
+            start = token.getStart();
+        }
+        if(end < token.getEnd()){
+            end = token.getEnd();
+        }
+    }
+
+    /**
+     * The POS tag of the Token with a sentiment.
+     * @return
+     */
+    public PosTag getPosTag() {
+        return posTag;
+    }
     public double getValue() {
         return negated == null ? value : value*-1;
     }
@@ -45,26 +132,46 @@ public class Sentiment {
     public Token getToken() {
         return token;
     }
+    public Sentence getSentence() {
+        return sentence;
+    }
+    public AnalysedText getAnalysedText(){
+        return token.getContext();
+    }
     
     public List<Token> getNegates() {
         return negated == null ? Collections.EMPTY_LIST : negated;
     }
 
-    public void noun(Token noun) {
-        if(nouns == null){
-            nouns = new ArrayList<Token>(4);
-        }
-        nouns.add(noun);
+    /**
+     * The Nouns or Pronoun(s) the Adjectives are about
+     * @return
+     */
+    public List<Token> getAboutness() {
+        return aboutness == null ? Collections.EMPTY_LIST : aboutness;
     }
-    public List<Token> getNouns() {
-        return nouns == null ? Collections.EMPTY_LIST : nouns;
+    /**
+     * The verb used to assign Adjectives to the Nouns (or Pronouns)
+     * @return
+     */
+    public Token getVerb() {
+        return verb;
+    }
+    
+    public int getStart(){
+        return start;
+    }
+    
+    public int getEnd(){
+        return end;
     }
     
     @Override
     public String toString() {
-        return new StringBuilder("Sentiment ").append(token.getSpan()).append('@')
+        return new StringBuilder("Sentiment [").append(start).append(',').append(end).append("]:")
+                .append(token).append('@')
                 .append(getValue()).append(" | negations: ").append(getNegates())
-                .append(" | nouns: ").append(getNouns()).toString();
+                .append(" | about: ").append(getAboutness()).append(" | verb: ").append(verb).toString();
     }
     @Override
     public int hashCode() {
@@ -78,4 +185,5 @@ public class Sentiment {
                 && !negated.isEmpty() && ((Sentiment)obj).negated != null &&
                 !((Sentiment)obj).negated.isEmpty()));
     }
+
 }
