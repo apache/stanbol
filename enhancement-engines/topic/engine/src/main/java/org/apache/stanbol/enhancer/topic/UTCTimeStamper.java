997623bb4ca15e9bdc8c8930d28f4aca668d18ac
Rupert Westenthaler <rwesten@apache.org>
2013-11-19 08:47:07 +0000

    STANBOL-1142: fixed private package decleration of the topic engine pom file in the trunk; also includes two formatting only changes in other files
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1543347 13f79535-47bb-0310-9956-ffa450edef68

--- enhancement-engines/topic/engine/src/main/java/org/apache/stanbol/enhancer/topic/UTCTimeStamper.java
+++ enhancement-engines/topic/engine/src/main/java/org/apache/stanbol/enhancer/topic/UTCTimeStamper.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.stanbol.enhancer.topic;
+ */
+package org.apache.stanbol.enhancer.topic;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
