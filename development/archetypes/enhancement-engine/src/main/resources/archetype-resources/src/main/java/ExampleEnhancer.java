3c0bc97f45238db8a3a3c6ffc2b6d1719c700188
Rupert Westenthaler <rwesten@apache.org>
2014-01-27 13:18:18 +0000

    STANBOL-1270: updates to the Enhancement Engine Archetype as descriped by the issue.
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1561664 13f79535-47bb-0310-9956-ffa450edef68

--- development/archetypes/enhancement-engine/src/main/resources/archetype-resources/src/main/java/ExampleEnhancer.java
+++ development/archetypes/enhancement-engine/src/main/resources/archetype-resources/src/main/java/ExampleEnhancer.java
@@ -5,14 +5,20 @@ package ${package};
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.UriRef;
 import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
 import org.apache.clerezza.rdf.core.impl.TripleImpl;
 import org.apache.clerezza.rdf.ontologies.DCTERMS;
 import org.apache.clerezza.rdf.ontologies.RDFS;
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
@@ -25,15 +31,22 @@ import org.apache.stanbol.enhancer.servicesapi.ServiceProperties;
 import org.apache.stanbol.enhancer.servicesapi.helper.ContentItemHelper;
 import org.apache.stanbol.enhancer.servicesapi.helper.EnhancementEngineHelper;
 import org.apache.stanbol.enhancer.servicesapi.impl.AbstractEnhancementEngine;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(immediate = true, metatype = true, inherit = true)
-@Service
-@Properties(value = {
-    @Property(name = EnhancementEngine.PROPERTY_NAME, value = "${artifactId}-example")
+@Component(configurationFactory = true, //allow multiple service instances (false for a  singelton instance)
+    policy = ConfigurationPolicy.OPTIONAL, //use REQUIRE if a non default option is present
+    immediate = true, //activate service instances on startup 
+    metatype = true, inherit = true, specVersion = "1.1")
+@Service //this will register the engine as an OSGI service
+@Properties(value = { //Configuration properties included in the config form
+    @Property(name = EnhancementEngine.PROPERTY_NAME, value = "${artifactId}-example"),
+    @Property(name = Constants.SERVICE_RANKING, intValue = 0)
 })
-public class ExampleEnhancer extends AbstractEnhancementEngine
+public class ExampleEnhancer extends AbstractEnhancementEngine<RuntimeException,RuntimeException>
         implements EnhancementEngine, ServiceProperties {
 
     /**
@@ -42,54 +55,108 @@ public class ExampleEnhancer extends AbstractEnhancementEngine
     private static final Logger log = LoggerFactory.getLogger(ExampleEnhancer.class);
 
     /**
+     * TODO: change to fit your engine. See constants defined in the 
+     * ServiceProperties class
+     */
+    protected static final Integer ENGINE_ORDERING = ServiceProperties.ORDERING_PRE_PROCESSING;
+    
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     * OSGI lifecycle methods
+     * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     */
+    /**
+     * Activate and read the properties. Configures and initialises a POSTagger for each language configured in
+     * CONFIG_LANGUAGES.
+     *
+     * @param ce the {@link org.osgi.service.component.ComponentContext}
+     */
+    @Activate
+    protected void activate(ComponentContext ce) throws ConfigurationException {
+        super.activate(ce);
+        log.info("activating {}: {}", getClass().getSimpleName(), getName());
+        @SuppressWarnings("unchecked")
+        Dictionary<String, Object> properties = ce.getProperties();
+        //TODO: parse custom properties
+    }
+    
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+        log.info("deactivating {}: {}", getClass().getSimpleName(), getName());
+        //TODO: reset fields to default, close resources ...
+        super.deactivate(context);
+    }
+    
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     * ServiceProperties interface method
+     * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     */
+    
+    /**
      * ServiceProperties are currently only used for automatic ordering of the 
      * execution of EnhancementEngines (e.g. by the WeightedChain implementation).
      * Default ordering means that the engine is called after all engines that
      * use a value < {@link ServiceProperties#ORDERING_CONTENT_EXTRACTION}
      * and >= {@link ServiceProperties#ORDERING_EXTRACTION_ENHANCEMENT}.
      */
-    public Map getServiceProperties() {
-        return Collections.unmodifiableMap(Collections.singletonMap(
-                ENHANCEMENT_ENGINE_ORDERING, ORDERING_DEFAULT));
+    public Map<String,Object> getServiceProperties() {
+        return Collections.unmodifiableMap(Collections.<String,Object>singletonMap(
+                ENHANCEMENT_ENGINE_ORDERING, ENGINE_ORDERING));
     }
+    
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     * EnhancementEngine interface methods
+     * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     */
 
     /**
      * @return if and how (asynchronously) we can enhance a ContentItem
      */
     public int canEnhance(ContentItem ci) throws EngineException {
-        // check if content is present
-        try {
-            if ((ci.getBlob() == null)
-                    || (ci.getBlob().getStream().read() == -1)) {
-                return CANNOT_ENHANCE;
-            }
-        } catch (IOException e) {
-            log.error("Failed to get the text for "
-                    + "enhancement of content: " + ci.getUri(), e);
-            throw new InvalidContentException(this, ci, e);
+        // check if a Content in the supported type is available
+        //NOTE: you can parse multiple content types
+        Entry<UriRef,Blob> textBlob = ContentItemHelper.getBlob(
+            ci, Collections.singleton("text/plain"));
+        if(textBlob == null) {
+            return CANNOT_ENHANCE;
         }
+        //TODO: test additional requirements for this EnhancementEngine
+
         // no reason why we should require to be executed synchronously
         return ENHANCE_ASYNC;
     }
 
     public void computeEnhancements(ContentItem ci) throws EngineException {
+        //(1) retrieve the required data from the ConentItem
+        String content;
         try {
             //get the (generated or submitted) text version of the ContentItem
-            Blob textBlob =
-                    ContentItemHelper.getBlob(ci,
+            Blob textBlob = ContentItemHelper.getBlob(ci,
                     Collections.singleton("text/plain")).getValue();
-            String content = ContentItemHelper.getText(textBlob);
-            // get the metadata graph
-            MGraph metadata = ci.getMetadata();
-            // update some sample data
+            //Blob provides an InputStream. Use the Utility to load the String
+            content = ContentItemHelper.getText(textBlob);
+        } catch (IOException ex) {
+            log.error("Exception reading content item.", ex);
+            throw new InvalidContentException("Exception reading content item.", ex);
+        }
+        
+        //(2) compute the enhancements
+        int contentLength = content.length();
+        
+        //(3) write the enhancement results        
+        // get the metadata graph
+        MGraph metadata = ci.getMetadata();
+        //NOTE: as we allow synchronous calls we need to use read/write
+        // locks on the ContentItem
+        ci.getLock().writeLock().lock();
+        try {
+            // TODO: replace this with real enhancements
             UriRef textAnnotation = EnhancementEngineHelper.createTextEnhancement(ci, this);
             metadata.add(new TripleImpl(textAnnotation, DCTERMS.type, 
                     new UriRef("http://example.org/ontology/LengthEnhancement")));
             metadata.add(new TripleImpl(textAnnotation, RDFS.comment,
-                    new PlainLiteralImpl("A text of " + content.length() + " charaters")));
-        } catch (IOException ex) {
-            log.error("Exception reading content item.", ex);
-            throw new InvalidContentException("Exception reading content item.", ex);
+                    new PlainLiteralImpl("A text of " + contentLength + " charaters")));
+        } finally {
+            ci.getLock().writeLock().unlock();
         }
     }
 }
