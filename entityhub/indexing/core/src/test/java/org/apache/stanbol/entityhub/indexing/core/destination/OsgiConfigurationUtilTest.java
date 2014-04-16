653c0350d20ecc64139d91ea92213a3a375b74a8
Rupert Westenthaler <rwesten@apache.org>
2014-04-16 07:49:16 +0000

    applied patch for STANBOL-1304 (the patch added a unit test for the repored issue); fixed the issue and also fixed the assertion for the created bundle in the unit test provided by the patch
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/branches/release-0.12@1587824 13f79535-47bb-0310-9956-ffa450edef68

---
+++ entityhub/indexing/core/src/test/java/org/apache/stanbol/entityhub/indexing/core/destination/OsgiConfigurationUtilTest.java
@@ -0,0 +1,80 @@
+package org.apache.stanbol.entityhub.indexing.core.destination;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import static org.junit.Assert.*;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.stanbol.entityhub.indexing.core.IndexerFactory;
+import org.apache.stanbol.entityhub.indexing.core.config.IndexingConfig;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OsgiConfigurationUtilTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(OsgiConfigurationUtilTest.class);
+    private static final String CONFIG_ROOT = 
+        FilenameUtils.separatorsToSystem("testOsgiConfiguration/");
+
+    private static IndexerFactory factory;
+    /**
+     * mvn copies the resources in "src/test/resources" to target/test-classes.
+     * This folder is than used as classpath.<p>
+     * "/target/test-files/" does not exist, but is created by the
+     * {@link IndexingConfig}.
+     */
+    private static final String TEST_ROOT = 
+        FilenameUtils.separatorsToSystem("/target/test-files");
+    private static String  userDir;
+    private static String testRoot;
+    /**
+     * The methods resets the "user.dir" system property
+     */
+    @BeforeClass
+    public static void initTestRootFolder(){
+        String baseDir = System.getProperty("basedir");
+        if(baseDir == null){
+            baseDir = System.getProperty("user.dir");
+        }
+        //store the current user.dir
+        userDir = System.getProperty("user.dir");
+        testRoot = baseDir+TEST_ROOT;
+        log.info("ConfigTest Root : "+testRoot);
+        //set the user.dir to the testRoot (needed to test loading of missing
+        //configurations via classpath
+        //store the current user.dir and reset it after the tests
+        System.setProperty("user.dir", testRoot);
+    }
+    
+	@Test
+	public void testOsgiConfigurationGeneration() throws URISyntaxException, IOException{
+		String name = CONFIG_ROOT+"bundlebuild";
+		
+		//copy test destination folder to mock-up the destination "config" folder creation operation
+		String destConfFolder = name + "/indexing/destination/" + "config";
+		File destConfSource = new File( this.getClass().getResource("/" + destConfFolder).toURI());
+		File destConfTarget = new File(testRoot + "/" + destConfFolder);
+		FileUtils.copyDirectory( destConfSource, destConfTarget);
+		
+		File distFolder = new File(testRoot+'/'+name+"/indexing/dist");
+		File bundle = new File(distFolder,"org.apache.stanbol.data.site.simple-1.0.0.jar");
+		
+        IndexingConfig config = new IndexingConfig(name,name){};
+        
+        OsgiConfigurationUtil.createBundle(config);
+        
+        assertTrue("Configuration bundle '"+bundle+"' was not created!", bundle.isFile());
+        
+        //if choose to get .createBundle throwing exceptions, this test may be better :
+//        try{
+//        	OsgiConfigurationUtil.createBundle(config);
+//        }catch(Exception npe){
+//        	fail("NPE fired when try to build the osgi configuration");
+//        }
+        
+	}
+}
