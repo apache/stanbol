e2fde5f00dfe266bbae7a6396eedeab567adb423
Rupert Westenthaler <rwesten@apache.org>
2014-10-27 15:43:12 +0000

    merged updates to STANBOL-1397 from trunk to 0.12.1 branch
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/branches/release-0.12@1634578 13f79535-47bb-0310-9956-ffa450edef68

--- enhancement-engines/nlp2rdf/src/main/java/org/apache/stanbol/enhancer/engines/nlp2rdf/engine/Nif20MetadataEngine.java
+++ enhancement-engines/nlp2rdf/src/main/java/org/apache/stanbol/enhancer/engines/nlp2rdf/engine/Nif20MetadataEngine.java
@@ -17,6 +17,8 @@
 package org.apache.stanbol.enhancer.engines.nlp2rdf.engine;
 
 import static org.apache.stanbol.enhancer.nlp.utils.NlpEngineHelper.getAnalysedText;
+import static org.apache.stanbol.enhancer.servicesapi.helper.EnhancementEngineHelper.DEFAULT_PREFIX_SUFFIX_LENGTH;
+import static org.apache.stanbol.enhancer.servicesapi.rdf.Properties.RDF_TYPE;
 
 import java.util.Collections;
 import java.util.Dictionary;
@@ -28,6 +30,7 @@ import org.apache.clerezza.rdf.core.Language;
 import org.apache.clerezza.rdf.core.LiteralFactory;
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
 import org.apache.clerezza.rdf.core.impl.TripleImpl;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
@@ -39,6 +42,7 @@ import org.apache.stanbol.enhancer.nlp.NlpAnnotations;
 import org.apache.stanbol.enhancer.nlp.model.AnalysedText;
 import org.apache.stanbol.enhancer.nlp.model.Span;
 import org.apache.stanbol.enhancer.nlp.model.Span.SpanTypeEnum;
+import org.apache.stanbol.enhancer.nlp.model.annotation.Annotation;
 import org.apache.stanbol.enhancer.nlp.model.annotation.Value;
 import org.apache.stanbol.enhancer.nlp.nif.Nif20;
 import org.apache.stanbol.enhancer.servicesapi.ContentItem;
@@ -58,10 +62,68 @@ import org.slf4j.LoggerFactory;
     policy = ConfigurationPolicy.OPTIONAL) //create a default instance
 @Service
 @Properties(value={
-        @Property(name= EnhancementEngine.PROPERTY_NAME,value="nif20")
+        @Property(name=EnhancementEngine.PROPERTY_NAME,value="nif20"),
+        @Property(name=Nif20MetadataEngine.PROP_SELECTOR_STATE, 
+            boolValue=Nif20MetadataEngine.DEFAULT_SELECTOR_STATE),
+        @Property(name=Nif20MetadataEngine.PROP_CONTEXT_ONLY_URI_SCHEME, 
+            boolValue=Nif20MetadataEngine.DEFAULT_CONTEXT_ONLY_URI_SCHEME),
+        @Property(name=Nif20MetadataEngine.PROP_WRITE_STRING_TYPE, 
+            boolValue=Nif20MetadataEngine.DEFAULT_WRITE_STRING_TYPE),
+        @Property(name=Nif20MetadataEngine.PROP_HIERARCHY_LINKS_STATE,
+            boolValue=Nif20MetadataEngine.DEFAULT_HIERARCHY_LINKS_STATE),
+        @Property(name=Nif20MetadataEngine.PROP_PREVIOUSE_NEXT_LINKS_STATE, 
+            boolValue=Nif20MetadataEngine.DEFAULT_PREVIOUSE_NEXT_LINKS_STATE)
 })
 public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeException,RuntimeException> implements ServiceProperties{
 
+    /**
+     * Switch that allows to enable/disable writing of hierarchical links. This
+     * includes <code>olia:sentence</code>, <code>olia:superString</code> and
+     * <code>olia:subString</code> properties.
+     */
+    public final static String PROP_HIERARCHY_LINKS_STATE = "enhancer.engines.nlp2rdf.hierarchy";
+    /**
+     * switch that allows to enable/disable writing of next and previous links 
+     * between words, sentences ...
+     */
+    public final static String PROP_PREVIOUSE_NEXT_LINKS_STATE = "enhancer.engines.nlp2rdf.previousNext";
+    /**
+     * If enabled Selector related properties such as begin-/end-index, before/after, ...
+     * are written. If disabled only the URI is generated (which is sufficient for
+     * clients that know about the semantics of how the URI is build). Deactivating
+     * this will greatly decrease the triple count.
+     */
+    public final static String PROP_SELECTOR_STATE = "enhancer.engines.nlp2rdf.selector";
+    /**
+     * If enabled the {@link Nif20#URIScheme nif:URIScheme} ( typically 
+     * {@link Nif20#RFC5147String nif:RFC5147String}) type will only be added 
+     * for the {@link Nif20#Context nif:Context} and not
+     * all {@link Nif20#String nif:String} instances. If enabled clients need 
+     * follow the {@link Nif20#referenceContext nif:referenceContext} to the 
+     * {@link Nif20#Context nif:Context} for getting the used 
+     * {@link Nif20#URIScheme nif:URIScheme}<p>
+     */
+    public final static String PROP_CONTEXT_ONLY_URI_SCHEME = "enhancer.engines.nlp2rdf.cotextOnlyUriScheme";
+    /**
+     * If enabled the {@link Nif20#String nif:String} type is added to all written
+     * String. If disabled it is only written if their is no more specific type (
+     * such as {@link Nif20#Sentence nif:Sentence} or {@link Nif20#Word nif:Word}.
+     */
+    public final static String PROP_WRITE_STRING_TYPE = "enhancer.engines.nlp2rdf.writeStringType";
+    
+    public static final boolean DEFAULT_HIERARCHY_LINKS_STATE = true;
+    public static final boolean DEFAULT_PREVIOUSE_NEXT_LINKS_STATE = true;
+    public static final boolean DEFAULT_SELECTOR_STATE = true;
+    public static final boolean DEFAULT_CONTEXT_ONLY_URI_SCHEME = false;
+    public static final boolean DEFAULT_WRITE_STRING_TYPE = false; 
+
+    
+    private boolean writeHierary = DEFAULT_HIERARCHY_LINKS_STATE;
+    private boolean writePrevNext = DEFAULT_PREVIOUSE_NEXT_LINKS_STATE;
+    private boolean writeSelectors = DEFAULT_SELECTOR_STATE;
+    private boolean contextOnlyUriScheme = DEFAULT_CONTEXT_ONLY_URI_SCHEME;
+    private boolean writeStringType = DEFAULT_WRITE_STRING_TYPE;
+    
     private final Logger log = LoggerFactory.getLogger(Nif20MetadataEngine.class);
     //TODO: replace this with a reald ontology
     private final static UriRef SENTIMENT_PROPERTY = new UriRef(NamespaceEnum.fise+"sentiment-value");
@@ -78,10 +140,23 @@ public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeExcept
         log.info("activating POS tagging engine");
         super.activate(ce);
         @SuppressWarnings("unchecked")
-        Dictionary<String, Object> properties = ce.getProperties();
-        //TODO: read configuration
+        Dictionary<String, Object> props = ce.getProperties();
+        writeHierary = getState(props, PROP_HIERARCHY_LINKS_STATE, DEFAULT_HIERARCHY_LINKS_STATE);
+        writePrevNext = getState(props, PROP_PREVIOUSE_NEXT_LINKS_STATE, DEFAULT_PREVIOUSE_NEXT_LINKS_STATE);
+        writeSelectors = getState(props, PROP_SELECTOR_STATE, DEFAULT_SELECTOR_STATE);
+        contextOnlyUriScheme = getState(props, PROP_CONTEXT_ONLY_URI_SCHEME, DEFAULT_CONTEXT_ONLY_URI_SCHEME);
+        writeStringType = getState(props, PROP_WRITE_STRING_TYPE, DEFAULT_WRITE_STRING_TYPE);
     }
     
+    private boolean getState(Dictionary<String, Object> props,
+            String prop, boolean def) {
+        Object val = props.get(prop);
+        boolean state = val == null ? def : val instanceof Boolean ? ((Boolean)val).booleanValue() :
+            Boolean.parseBoolean(val.toString());
+        log.debug(" - {}: {}",prop,state);
+        return state;
+    }
+
     @Override
     public int canEnhance(ContentItem ci) throws EngineException {
         return getAnalysedText(this, ci, false) != null ? 
@@ -113,10 +188,10 @@ public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeExcept
         UriRef base = ci.getUri();
         ci.getLock().writeLock().lock();
         try {
-        	//write the context
-        	UriRef text = Nif20Helper.writeSpan(metadata, base, at, language, at);
-    		metadata.add(new TripleImpl(text, Nif20.sourceUrl.getUri(), ci.getUri()));
-        	
+            //write the context
+            UriRef text = writeSpan(metadata, base, at, language, at);
+            metadata.add(new TripleImpl(text, Nif20.sourceUrl.getUri(), ci.getUri()));
+            
             Iterator<Span> spans = at.getEnclosed(activeTypes);
             UriRef sentence = null;
             UriRef phrase = null;
@@ -126,39 +201,43 @@ public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeExcept
                 Span span = spans.next();
                 //TODO: filter Spans based on additional requirements
                 //(1) write generic information about the span
-                UriRef current = Nif20Helper.writeSpan(metadata, base, at, language, span);
+                UriRef current = writeSpan(metadata, base, at, language, span);
                 //write the context
                 metadata.add(new TripleImpl(current, Nif20.referenceContext.getUri(), text));
                 //(2) add the relations between the different spans
                 switch (span.getType()) {
                     case Sentence:
-                        if(sentence != null){
+                        if(sentence != null && writePrevNext){
                             metadata.add(new TripleImpl(sentence, Nif20.nextSentence.getUri(), current));
+                            metadata.add(new TripleImpl(current, Nif20.previousSentence.getUri(), sentence));
+                        }
+                        if(word != null){
+                            metadata.add(new TripleImpl(sentence, Nif20.lastWord.getUri(), word));
                         }
                         sentence = current;
                         firstWordInSentence = true;
                         break;
                     case Chunk:
-                        if(sentence != null){
+                        if(sentence != null && writeHierary){
                             metadata.add(new TripleImpl(current, Nif20.superString.getUri(), sentence));
-                            if(word != null){
-                                metadata.add(new TripleImpl(word, Nif20.lastWord.getUri(), sentence));
-                            }
                         }
                         phrase = current;
                         break;
                     case Token:
                         if(sentence != null){
-                            metadata.add(new TripleImpl(current, Nif20.sentence.getUri(), sentence));
+                            if(writeHierary){
+                                metadata.add(new TripleImpl(current, Nif20.sentence.getUri(), sentence));
+                            }
+                            //metadata.add(new TripleImpl(sentence, Nif20.word.getUri(), current));
                             if(firstWordInSentence){
-                                metadata.add(new TripleImpl(current, Nif20.firstWord.getUri(), sentence));
+                                metadata.add(new TripleImpl(sentence, Nif20.firstWord.getUri(), current));
                                 firstWordInSentence = false;
                             }
                         }
-                        if(phrase != null){
+                        if(writeHierary && phrase != null && !phrase.equals(current)){
                             metadata.add(new TripleImpl(current, Nif20.subString.getUri(), phrase));
                         }
-                        if(word != null){
+                        if(word != null && writePrevNext){
                             metadata.add(new TripleImpl(word, Nif20.nextWord.getUri(), current));
                             metadata.add(new TripleImpl(current, Nif20.previousWord.getUri(), word));
                         }
@@ -168,10 +247,10 @@ public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeExcept
                         break;
                 }
                 //(3) add specific information such as POS, chunk type ...
-                Nif20Helper.writePos(metadata, span, current);
                 Nif20Helper.writePhrase(metadata, span, current);
+                Nif20Helper.writePos(metadata, span, current);
 
-                //OlIA does not include Sentiments
+                //TODO: sentiment support
                 
                 Value<Double> sentiment = span.getAnnotation(NlpAnnotations.SENTIMENT_ANNOTATION);
                 if(sentiment != null && sentiment.value() != null){
@@ -190,7 +269,74 @@ public class Nif20MetadataEngine extends AbstractEnhancementEngine<RuntimeExcept
             (Object)ServiceProperties.ORDERING_POST_PROCESSING);
     }
 
-
+    /**
+     * Writes basic information of the parsed span by using NIF 1.0 including the
+     * {@link SsoOntology} Sentence/Phrase/Word type based on 
+     * the {@link Span#getType()}<p>
+     * As {@link AnalysedText} is based on the plain text version of the ContentItem
+     * this uses the {@link StringOntology#OffsetBasedString} notation.<p>
+     * <i>NOTE:</i> This DOES NOT write string relations, lemma, pos ... information
+     * that might be stored as {@link Annotation} with the parsed {@link Span}.
+     * @param graph the graph to add the triples
+     * @param base the base URI
+     * @param text the {@link AnalysedText}
+     * @param language the {@link Language} or <code>null</code> if not known
+     * @param span the {@link Span} to write.
+     * @return the {@link UriRef} representing the parsed {@link Span} in the
+     * graph
+     */
+    public UriRef writeSpan(MGraph graph, UriRef base, AnalysedText text, Language language, Span span){
+        UriRef segment = Nif20Helper.getNifRFC5147URI(base, span.getStart(), 
+                span.getType() == SpanTypeEnum.Text ? -1 : span.getEnd());
+        if(!contextOnlyUriScheme || span.getType() == SpanTypeEnum.Text){
+            graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.RFC5147String.getUri()));
+        }
+        if(writeSelectors){
+            if(span.getEnd() - span.getStart() < 100){
+                graph.add(new TripleImpl(segment, Nif20.anchorOf.getUri(), 
+                    new PlainLiteralImpl(span.getSpan(),language)));
+            } else {
+                graph.add(new TripleImpl(segment, Nif20.head.getUri(), 
+                    new PlainLiteralImpl(span.getSpan().substring(0,10),language)));
+            }
+            graph.add(new TripleImpl(segment, Nif20.beginIndex.getUri(), 
+                lf.createTypedLiteral(span.getStart())));
+            graph.add(new TripleImpl(segment, Nif20.endIndex.getUri(), 
+                lf.createTypedLiteral(span.getEnd())));
+            String content = text.getSpan();
+            if(span.getType() != SpanTypeEnum.Text){
+                //prefix and suffix
+                int prefixStart = Math.max(0, span.getStart() - DEFAULT_PREFIX_SUFFIX_LENGTH);
+                graph.add(new TripleImpl(segment, Nif20.before.getUri(), new PlainLiteralImpl(
+                        content.substring(prefixStart, span.getStart()), language)));
+                int suffixEnd = Math.min(span.getEnd() + DEFAULT_PREFIX_SUFFIX_LENGTH, text.getEnd());
+                graph.add(new TripleImpl(segment, Nif20.after.getUri(), new PlainLiteralImpl(
+                        content.substring(span.getEnd(), suffixEnd), language)));
+            }
+        }
+        if(writeStringType){
+            graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.String.getUri()));
+        }
+        switch (span.getType()) {
+            case Token:
+                graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.Word.getUri()));
+                break;
+            case Chunk:
+                graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.Phrase.getUri()));
+                break;
+            case Sentence:
+                graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.Sentence.getUri()));
+                break;
+            case Text:
+                graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.Context.getUri()));
+                break;
+            default:
+                if(!writeStringType){
+                    graph.add(new TripleImpl(segment, RDF_TYPE, Nif20.String.getUri()));
+                } //string type was already added
+        }
+        return segment;
+    }
 
 
 
