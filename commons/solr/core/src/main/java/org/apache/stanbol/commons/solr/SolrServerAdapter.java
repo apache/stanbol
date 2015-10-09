6c0b3a4315595ba4db8c8809fe8791170c01b86b
Rupert Westenthaler <rwesten@apache.org>
2015-10-09 07:04:05 +0000

    nerged fix for STANBOL-1442 to trunk
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1707666 13f79535-47bb-0310-9956-ffa450edef68

--- commons/solr/core/src/main/java/org/apache/stanbol/commons/solr/SolrServerAdapter.java
+++ commons/solr/core/src/main/java/org/apache/stanbol/commons/solr/SolrServerAdapter.java
@@ -192,7 +192,7 @@ public class SolrServerAdapter {
             try {
                 updateServerRegistration();
             } catch (IllegalStateException e) {
-                log.debug("Server Registration already unregistered ",e);
+                log.debug("Not updating Server Registration (already unregistered)");
             }
         }
 
