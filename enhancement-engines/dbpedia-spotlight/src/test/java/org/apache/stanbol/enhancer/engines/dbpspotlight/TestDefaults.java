64913c36b1736c6c9c1a9e1423999d9417bf5e26
Rupert Westenthaler <rwesten@apache.org>
2016-02-23 09:17:16 +0000

    STANBOL-1449: adapted the tests so that they do no longer use the public service URL that is no longer available)
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/branches/release-0.12@1731812 13f79535-47bb-0310-9956-ffa450edef68

---
+++ enhancement-engines/dbpedia-spotlight/src/test/java/org/apache/stanbol/enhancer/engines/dbpspotlight/TestDefaults.java
@@ -0,0 +1,9 @@
+package org.apache.stanbol.enhancer.engines.dbpspotlight;
+
+public interface TestDefaults {
+
+    //TODO: add here a public service that can be used for tests if one becomes
+    //      available again
+    String DEFAULT_SPL_URL = null;
+    
+}
